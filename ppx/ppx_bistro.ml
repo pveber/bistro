1270464a69e1f668617905079e1867921af1d3e5
Philippe Veber <philippe.veber@gmail.com>
2015-08-26 00:08:52 +0200

    ppx: better (but still not perfect) location in antiquotations
    When parsing an antiquotation, the (ocaml) expression parser is given
    the location of the whole extension. Before this fix, the reported
    location was inside the string containing the antiquotation. To have
    even better location, the (script) AST should contain location info
    from the Lexing module. This should be feasible by using an ocamllex
    lexer.

--- ppx/ppx_bistro.ml
+++ ppx/ppx_bistro.ml
@@ -20,12 +20,14 @@ let parse =
     Str.full_split re s
     |> List.map f
 
-let script_of_ast ast =
+let script_of_ast loc ast =
   let f = function
     | Text s -> [%expr string [%e Ast_convenience.str s] ]
     | Antiquot "DEST" -> [%expr dest ]
     | Antiquot "TMP" -> [%expr tmp ]
-    | Antiquot e -> Parse.expression (Lexing.from_string e)
+    | Antiquot e ->
+      let buf = Lexing.from_string e in
+      Parse.expression { buf with Lexing.lex_curr_p = loc.Location.loc_start }
   in
   Ast_convenience.list (List.map f ast)
 
@@ -55,7 +57,7 @@ let bistro_mapper argv =
           | PStr [{ pstr_desc = Pstr_eval ({ pexp_loc  = loc;
                                              pexp_desc = Pexp_constant (Const_string (sym, _))}, _)}] ->
             Ast_helper.default_loc := loc ;
-            [%expr let open Bistro.Script in make [%e interpreter_of_ext ext_name ] [%e script_of_ast (parse sym)] ]
+            [%expr let open Bistro.Script in make [%e interpreter_of_ext ext_name ] [%e script_of_ast loc (parse sym)] ]
           | _ ->
             raise (Location.Error (
                 Location.error ~loc "[%bistro] only accepts a string"))
