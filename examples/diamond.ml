e13937b0047d9036fc150443856f7d62864b85fe
Philippe Veber <philippe.veber@gmail.com>
2015-08-26 09:16:47 +0200

    examples/diamond: fixed typo

--- examples/diamond.ml
+++ examples/diamond.ml
@@ -11,13 +11,13 @@ sleep 2
 
 let b : txt workflow = Workflow.make [%bistro.sh {|
 echo This is b > {{DEST}}
-cat {{dep a}} > {{DEST}}
+cat {{dep a}} >> {{DEST}}
 date >> {{DEST}}
 |}]
 
 let c : txt workflow = Workflow.make [%bistro.sh {|
 echo This is c > {{DEST}}
-cat {{dep a}} > {{DEST}}
+cat {{dep a}} >> {{DEST}}
 date >> {{DEST}}
 |}]
 
@@ -30,7 +30,17 @@ let e = Engine.make ~np:1 ~mem:1024 db
 
 let main () =
   Engine.build_exn e d >>= fun s ->
-  print_endline (In_channel.read_all s) ;
+  print_endline "Contents of a:" ;
+  print_endline (In_channel.read_all (Db.workflow_path db a)) ;
+  print_newline () ;
+  print_endline "Contents of b:" ;
+  print_endline (In_channel.read_all (Db.workflow_path db b)) ;
+  print_newline () ;
+  print_endline "Contents of c:" ;
+  print_endline (In_channel.read_all (Db.workflow_path db c)) ;
+  print_newline () ;
+  print_endline "Contents of d:" ;
+  print_endline (In_channel.read_all (Db.workflow_path db d)) ;
   Lwt.return ()
 
 let () = Lwt_unix.run (main ())
