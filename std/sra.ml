b066d0162d5063acb67d4fdd431e7457b6221323
pveber <philippe.veber@gmail.com>
2015-09-23 09:33:59 +0200

    Sra.fetch_srr: put fetched file in DEST

--- std/sra.ml
+++ std/sra.ml
@@ -9,7 +9,7 @@ let fetch_srr id =
   if (String.length id > 6) then (
     let prefix = String.prefix id 6 in
     workflow ~descr:(sprintf "sra.fetch_srr(%s)" id) [
-      wget (sprintf "ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/%s/%s/%s.sra" prefix id id) ()
+      wget ~dest (sprintf "ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/%s/%s/%s.sra" prefix id id) ()
     ]
   )
   else failwithf "Guizmin_workflow.Sra.fetch_srr: id %s is invalid (should be longer than 6 characters long)" id ()
