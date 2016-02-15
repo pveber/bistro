d7573319ae1b7a652c8206ba1adeb51ba9380727
Philippe Veber <philippe.veber@gmail.com>
2016-02-15 05:27:51 +0100

    server: minimal skel for req routing

--- app/bistro_server.ml
+++ app/bistro_server.ml
@@ -1,17 +1,25 @@
+open Core.Std
 open Lwt
 open Cohttp
 open Cohttp_lwt_unix
 
+let path_of_uri uri =
+  match String.split ~on:'/' (Uri.path uri) with
+  | "" :: q -> q
+  | p -> p
+
+let callback _conn req body =
+  let uri = Request.uri req in
+  let meth = Request.meth req in
+  match meth, path_of_uri uri with
+  | `POST, [ "wave" ] ->
+    Server.respond_string ~status:`OK ~body:"" ()
+  | _ ->
+    Server.respond_string ~status:`Not_found ~body:"Page not found!" ()
+
 let server =
-  let callback _conn req body =
-    let uri = req |> Request.uri |> Uri.to_string in
-    let meth = req |> Request.meth |> Code.string_of_method in
-    let headers = req |> Request.headers |> Header.to_string in
-    body |> Cohttp_lwt_body.to_string >|= (fun body ->
-      (Printf.sprintf "Uri: %s\nMethod: %s\nHeaders\nHeaders: %s\nBody: %s"
-         uri meth headers body))
-    >>= (fun body -> Server.respond_string ~status:`OK ~body ())
-  in
-  Server.create ~mode:(`TCP (`Port 8000)) (Server.make ~callback ())
+  Server.create
+    ~mode:(`TCP (`Port 8000))
+    (Server.make ~callback ())
 
 let () = ignore (Lwt_main.run server)
