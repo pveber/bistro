51d82711046bdd75eab278c447e7ecd554a3e0cf
Philippe Veber <philippe.veber@gmail.com>
2016-02-15 15:33:16 +0100

    resurrected test for failures

--- test/fail.ml
+++ test/fail.ml
@@ -1,23 +1,21 @@
-open Bistro_std
-open Lwt
-
-module Workflow = Bistro.Workflow
-module Engine = Bistro.Engine
-module Db = Bistro.Db
+open Bistro.Std
+open Bistro
 
 type failure
 
-let fail : failure workflow = Workflow.make [%sh{|
+let fail1 : failure workflow = Workflow.make [%sh{|
 false
 |}]
 
-let db = Db.init_exn "_bistro"
-let e = Engine.make ~np:2 ~mem:1024 db
+let fail2 : failure workflow = Workflow.make [%sh{|
+echo
+|}]
 
-let main () =
-  Engine.build_exn e fail >>= fun s ->
-  Lwt.return ()
 
 let () =
-  try Lwt_unix.run (main ())
-  with Failure s -> print_endline s
+  Bistro_app.(
+    simple [
+      [ "output" ; "fail1" ] %> fail1 ;
+      [ "output" ; "fail2" ] %> fail2 ;
+    ]
+  )
