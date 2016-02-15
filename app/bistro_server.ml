076a18982c7664cf3ae4abd7f63e6fea7b8cca06
Philippe Veber <philippe.veber@gmail.com>
2016-02-15 05:46:11 +0100

    server: implemented post wave handler

--- app/bistro_server.ml
+++ app/bistro_server.ml
@@ -2,6 +2,17 @@ open Core.Std
 open Lwt
 open Cohttp
 open Cohttp_lwt_unix
+open Bistro_engine
+
+let db = Db.init_exn "_bistro"
+
+let post_wave wave_name body =
+  try
+    let wave = Db.Wave.of_string body in
+    Db.Wave_table.set db wave_name wave ;
+    return (`OK, "")
+  with
+  | e -> return (`Bad_request, Exn.to_string e)
 
 let path_of_uri uri =
   match String.split ~on:'/' (Uri.path uri) with
@@ -11,11 +22,16 @@ let path_of_uri uri =
 let callback _conn req body =
   let uri = Request.uri req in
   let meth = Request.meth req in
-  match meth, path_of_uri uri with
-  | `POST, [ "wave" ] ->
-    Server.respond_string ~status:`OK ~body:"" ()
-  | _ ->
-    Server.respond_string ~status:`Not_found ~body:"Page not found!" ()
+  Cohttp_lwt_body.to_string body >>= fun body ->
+  (
+    match meth, path_of_uri uri with
+    | `POST, [ "wave" ; wave_name ] ->
+      post_wave wave_name body
+    | _ ->
+      return (`Not_found, "Page not found!")
+  )
+  >>= fun (status, body) ->
+  Server.respond_string ~status ~body ()
 
 let server =
   Server.create
