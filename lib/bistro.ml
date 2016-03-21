pveber/bistro

commit 3e468fe324598d7fd5568d9b4299f89ff3250144

Introduction of packages-as-external-dependencies
With this commit, I start removing workflows corresponding to
installation scripts, as this is out-of-scope of the library. To keep
track of programs that should be available for a workflow to be run, I
introduce a `package` type that encodes the name and version of a
necessary program. Values of type `package` can be used in scripts,
thanks to a new token. It is up to each backend to provide the
packages. For now, we just assume `packages` are in the PATH.

Workflows corresponding to installation scripts will progressively be
removed.

lib/bistro.ml
@@ -55,6 +55,7 @@ module T = struct
   and token =
     | S of string
     | D of u
+    | PKGVAR of package * package_variable
     | DEST
     | TMP
     | NP
@@ -70,7 +71,20 @@ module T = struct
     | `sh
   ]
 
+  and package = {
+    pkg_name : string ;
+    pkg_version : string ;
+  }
+
+  and package_variable = [ `bin | `inc | `lib | `share ]
+
   with sexp
+
+  let string_of_package_variable = function
+    | `bin -> "bin"
+    | `lib -> "lib"
+    | `inc -> "include"
+    | `share -> "share"
 end
 
 include T
@@ -88,20 +102,21 @@ module Script = struct
   let deps s =
     List.filter_map s.tokens ~f:(function
         | D r -> Some (r :> u)
-        | S _ | DEST | TMP | NP | MEM -> None
+        | S _ | DEST | TMP | NP | MEM | PKGVAR _ -> None
       )
     |> List.dedup
 
-  let string_of_token ~string_of_workflow ~tmp ~dest ~np ~mem = function
+  let string_of_token ~string_of_workflow ~tmp ~dest ~pkgvar ~np ~mem = function
     | S s -> s
     | D w -> string_of_workflow (w :> u)
     | DEST -> dest
     | TMP -> tmp
     | NP -> string_of_int np
     | MEM -> string_of_int mem
+    | PKGVAR (pkg, var) -> pkgvar pkg var
 
-  let to_string ~string_of_workflow ~tmp ~dest ~np ~mem script =
-    let f = string_of_token ~string_of_workflow ~tmp ~dest ~np ~mem in
+  let to_string ~string_of_workflow ~tmp ~dest ~pkgvar ~np ~mem script =
+    let f = string_of_token ~string_of_workflow ~tmp ~dest ~pkgvar ~np ~mem in
     List.map script.tokens ~f
     |> String.concat
 end
@@ -188,8 +203,6 @@ module Workflow = struct
 end
 
 type 'a directory = [`directory of 'a]
-type package = [`package] directory
-
 
 module EDSL = struct
   type expr = token list
@@ -210,6 +223,7 @@ module EDSL = struct
   let float f = [ S (Float.to_string f) ]
   let path p = [ S (string_of_path p) ]
   let dep w = [ D w ]
+  let pkgvar pkg var = [ PKGVAR (pkg, var) ]
 
   let quote ?(using = '"') e =
     let quote_symbol = Char.to_string using in
@@ -368,7 +382,6 @@ module Std = struct
   end
 
   type 'a directory = [`directory of 'a]
-  type package = [`package] directory
   type 'a zip = ([`zip of 'a], [`binary]) file
   type 'a gz = ([`gz of 'a], [`binary]) file constraint 'a = (_,_) #file
   type 'a bz2 = ([`bz2 of 'a], [`binary]) file constraint 'a = (_,_) #file

lib/bioinfo/bedtools.mli
@@ -1,4 +1,4 @@
 open Bistro.Std
 open Types
 
-val package : package workflow
+val package : [`package] workflow

lib/bioinfo/bowtie.ml
@@ -19,7 +19,7 @@ mv bowtie-1.0.1 bin
 
 (* memory bound correspond to storing a human index in memory, following bowtie manual *)
 let bowtie_build ?packed ?color fa =
-  workflow ~descr:"bowtie_build" ~mem:(3 * 1024) ~timeout:24 [
+  workflow ~descr:"bowtie_build" ~mem:(3 * 1024) [
     mkdir_p dest ;
     cmd "bowtie-build" ~path:[package] [
       option (flag string "-a -p") packed ;
@@ -44,7 +44,7 @@ let bowtie ?l ?e ?m ?fastq_format ?n ?v ?p ?maxins index fastq_files =
         opt "-2" (list dep ~sep:",") fqs2
       ]
   in
-  workflow ~descr:"bowtie" ~mem:(3 * 1024) ~timeout:24 ?np:p [
+  workflow ~descr:"bowtie" ~mem:(3 * 1024) ?np:p [
     cmd "bowtie" ~path:[package] [
       string "-S" ;
       option (opt "-n" int) n ;

lib/bioinfo/bowtie.mli
@@ -3,7 +3,7 @@ open Types
 
 type index = [`bowtie_index] directory
 
-val package : package workflow
+val package : [`package] workflow
 
 val bowtie_build :
   ?packed:bool ->

lib/bioinfo/bowtie2.ml
@@ -18,7 +18,7 @@ mv bowtie2-2.2.4 bin
 
 (* memory bound correspond to storing a human index in memory, following bowtie manual *)
 let bowtie2_build ?large_index ?noauto ?packed ?bmax ?bmaxdivn ?dcv ?nodc ?noref ?justref ?offrate ?ftabchars ?seed ?cutoff fa =
-  workflow ~descr:"bowtie2_build" ~mem:(3 * 1024) ~timeout:24 [
+  workflow ~descr:"bowtie2_build" ~mem:(3 * 1024) [
     mkdir_p dest ;
     cmd "bowtie2-build" ~path:[package] [
       option (flag string "--large-index") large_index ;
@@ -82,7 +82,7 @@ let bowtie2
         opt "-2" (list dep ~sep:",") fqs2
       ]
   in
-  workflow ~descr:"bowtie2" ~mem:(3 * 1024) ~timeout:24 ?np:threads [
+  workflow ~descr:"bowtie2" ~mem:(3 * 1024) ?np:threads [
     cmd "bowtie2" ~path:[package] [
       option (opt "--skip" int) skip ;
       option (opt "--qupto" int) qupto ;

lib/bioinfo/bowtie2.mli
@@ -1,7 +1,7 @@
 open Bistro.Std
 open Types
 
-val package : package workflow
+val package : [`package] workflow
 
 type index = [`bowtie2_index] directory
 

lib/bioinfo/fastQC.mli
@@ -1,7 +1,7 @@
 open Bistro.Std
 open Types
 
-val package : package workflow
+val package : [`package] workflow
 
 type report = [`fastQC_report] directory
 

lib/bioinfo/macs.mli
@@ -1,6 +1,6 @@
 open Bistro.Std(* open Bistro_workflow.Types *)
 
-(* val package : package workflow *)
+(* val package : [`package] workflow *)
 
 (* module Xls : sig *)
 (*   type 'a header = (string * (int * (int * (int * (int * (int * (float * (float * 'a)))))))) *)

commit aa427441e4f6af34875609a4fa7474488e214be4

fixed bug introduced in 3e468fe3

lib/bistro.ml
@@ -147,15 +147,22 @@ module Workflow = struct
       ?version
       script =
     let deps = Script.deps script in
-    let id = digest ("step",
-                     version,
-                     Script.to_string
-                       ~string_of_workflow:id
-                       ~np:1
-                       ~mem:1024
-                       ~tmp:"TMP"
-                       ~dest:"DEST"
-                       script) in
+    let script_as_string : string =
+      Script.to_string
+        ~string_of_workflow:id
+        ~pkgvar:(fun { pkg_name ; pkg_version } var ->
+            sprintf "%s:%s:%s"
+              pkg_name
+              pkg_version
+              (string_of_package_variable var)
+          )
+        ~np:1
+        ~mem:1024
+        ~tmp:"TMP"
+        ~dest:"DEST"
+        script
+    in
+    let id = digest ("step", version, script_as_string) in
     Step { descr ; deps ; script ; np ; mem ; timeout ; version ; id }
 
   let select u (Selector path) =