07591000dcbd9033536766ca2557734ae67a5f0c
Philippe Veber <philippe.veber@gmail.com>
2016-04-01 20:24:39 +0200

    bioinfo/FastQC: removed install script

--- lib/bioinfo/fastQC.ml
+++ lib/bioinfo/fastQC.ml
@@ -2,57 +2,23 @@ open Types
 open Bistro
 open Bistro.EDSL_sh
 
-let package = Workflow.make ~descr:"fastQC.package" [%sh{|
-PREFIX={{ dest }}
-TMP={{ tmp }}
-
-set -e
-
-URL=http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.10.1.zip
-ARCHIVE=`basename ${URL}`
-PACKAGE=${ARCHIVE%\.zip}
-
-cd $TMP
-wget -O ${ARCHIVE} ${URL}
-unzip ${ARCHIVE}
-
-cd FastQC
-mkdir -p ${PREFIX}/local/fastqc
-cp -r * ${PREFIX}/local/fastqc
-chmod 755 ${PREFIX}/local/fastqc/fastqc
-
-mkdir -p $PREFIX/bin
-cd $PREFIX/bin
-ln -s ../local/fastqc/fastqc .
-
-rm -rf $TMP
-|}]
+let package = {
+  pkg_name = "fastqc" ;
+  pkg_version = "0.10.1" ;
+}
 
 type report = [`fastQC_report] directory
 
-let run fq = Workflow.make ~descr:"fastQC" [%bash{|
+let run fq = Workflow.make ~pkgs:[package] ~descr:"fastQC" [%bash{|
 DEST={{ dest }}
 mkdir $DEST
 
 set -e
-export PATH={{ dep package }}/bin:$PATH
 fastqc --outdir=$DEST {{ dep fq }}
 rm -rf $DEST/*.zip
 mv $DEST/*_fastqc/* $DEST
 rm -rf $DEST/*_fastqc
 |}]
-
-(* [ *)
-(*     mkdir_p dest ; *)
-(*     cmd "fastqc" ~path:[package] [ *)
-(*       seq [string "--outdir=" ; dest] ; *)
-(*       dep fq ; *)
-(*     ] ; *)
-(*     rm_rf (dest // "*.zip") ; *)
-(*     mv (dest // "*_fastqc/*") (dest) ; *)
-(*     rm_rf (dest // "*_fastqc") ; *)
-(*   ] *)
-
 
 let html_report =
   selector ["fastqc_report.html"]
