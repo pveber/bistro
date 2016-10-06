3246eef6a4e286f2aca4e9f2f126ce257130f0a3
Philippe Veber <philippe.veber@gmail.com>
2016-10-06 21:24:50 +0200

    engine2/Allocator: expose create function

--- lib/engine2/allocator.mli
+++ lib/engine2/allocator.mli
@@ -1,4 +1,5 @@
 type t
+
 type request = Request of {
     np : int ;
     mem : int ;
@@ -9,5 +10,6 @@ type resource = Resource of {
     mem : int ;
   }
 
+val create : np:int -> mem:int -> t
 val request : t -> request -> resource Lwt.t
 val release : t -> resource -> unit
