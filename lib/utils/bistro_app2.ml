97e66f13a4665b7f7fd31bc5d287cbb9590f85cb
Philippe Veber <philippe.veber@gmail.com>
2016-10-07 22:58:07 +0200

    utils/App2: update wrt to new engine

--- lib/utils/bistro_app2.ml
+++ lib/utils/bistro_app2.ml
@@ -40,91 +40,68 @@ let rec eval : type s. Db.t -> s t -> s
       List.map xs ~f:(eval db)
 
 
-let rec string_of_path = function
-  | [] -> "."
-  | "" :: t -> Filename.concat "." (string_of_path t)
-  | p -> List.reduce_exn p ~f:Filename.concat
+let error_report_aux db = function
+  | tid, Tdag_sig.Run { outcome = Error (`Msg msg) } ->
+    fprintf stderr "################################################################################\n" ;
+    fprintf stderr "#                                                                              #\n" ;
+    fprintf stderr "#  Task %s failed\n" tid ;
+    fprintf stderr "#                                                                               \n" ;
+    fprintf stderr "#------------------------------------------------------------------------------#\n" ;
+    fprintf stderr "#                                                                               \n" ;
+    fprintf stderr "# %s\n" msg ;
+    fprintf stderr "#                                                                              #\n" ;
+    fprintf stderr "################################################################################\n" ;
+    fprintf stderr "###\n" ;
+    fprintf stderr "##    Report on %s \n" tid ;
+    fprintf stderr "#\n" ;
+    fprintf stderr "+------------------------------------------------------------------------------+\n" ;
+    fprintf stderr "| STDOUT                                                                       |\n" ;
+    fprintf stderr "+------------------------------------------------------------------------------+\n" ;
+    fprintf stderr "%s\n" (In_channel.read_all (Db.stdout db tid)) ;
+    fprintf stderr "+------------------------------------------------------------------------------+\n" ;
+    fprintf stderr "| STDERR                                                                       |\n" ;
+    fprintf stderr "+------------------------------------------------------------------------------+\n" ;
+    fprintf stderr "%s\n" (In_channel.read_all (Db.stderr db tid)) ;
+  | _ -> ()
 
-let error_report db xs =
-  List.iter xs ~f:(fun (dep, msg) ->
-      match dep with
-      | `Input i ->
-        fprintf stderr "################################################################################\n" ;
-        fprintf stderr "#                                                                              #\n" ;
-        fprintf stderr "#  Invalid input %s\n" (string_of_path i) ;
-        fprintf stderr "#                                                                              #\n" ;
-        fprintf stderr "# %s\n" msg ;
-        fprintf stderr "#                                                                              #\n" ;
-        fprintf stderr "################################################################################\n"
+let error_report db traces =
+  String.Map.to_alist traces
+  |> List.iter ~f:(error_report_aux db)
 
-      | `Select (tid, p) ->
-        fprintf stderr "################################################################################\n" ;
-        fprintf stderr "#                                                                              #\n" ;
-        fprintf stderr "#  Invalid select: no %s in %s\n" (string_of_path p) tid ;
-        fprintf stderr "#                                                                              #\n" ;
-        fprintf stderr "# %s\n" msg ;
-        fprintf stderr "#                                                                              #\n" ;
-        fprintf stderr "################################################################################\n" ;
 
-      | `Task tid ->
-        let report = match Db.Task_table.get db tid with
-          | Some t -> Db.report db t
-          | None -> sprintf "Unregistered task %s" tid
-        in
-        fprintf stderr "################################################################################\n" ;
-        fprintf stderr "#                                                                              #\n" ;
-        fprintf stderr "#  Task %s failed\n" tid ;
-        fprintf stderr "#                                                                               \n" ;
-        fprintf stderr "#------------------------------------------------------------------------------#\n" ;
-        fprintf stderr "#                                                                               \n" ;
-        fprintf stderr "# %s\n" msg ;
-        fprintf stderr "#                                                                              #\n" ;
-        prerr_endline report
+let has_error traces =
+  String.Map.exists traces ~f:(function
+      | Tdag_sig.Run { outcome = Error _ } -> true
+      | _ -> false
     )
 
-let collect_errors results =
-  List.map results ~f:(function
-      | Ok _ -> []
-      | Error xs -> xs
-    )
-  |> List.concat
-  |> List.dedup
-
-let run ?use_docker ?(np = 1) ?(mem = 1024) ?tmpdir app =
+let run ?(use_docker = true) ?(np = 1) ?(mem = 1024) app =
   let open Lwt in
-  let backend = Scheduler.local_backend ?use_docker ?tmpdir ~np ~mem () in
   let main =
-    let db = Db.init_exn "_bistro" in
-    let scheduler = Scheduler.make backend db in
+    let config = Task.config ~db_path:"_bistro" ~use_docker in
+    let allocator = Allocator.create ~np ~mem in
     let workflows = to_workflow_list app in
-    Scheduler.build_all scheduler workflows >|=
-    collect_errors >>= (
-      function
-      | [] -> return (eval db app)
-      | errors ->
-        error_report db errors ;
-        fail (Failure "Bistro_app failed!")
+    Scheduler.run config allocator workflows >>= fun traces ->
+    if has_error traces then (
+      error_report config.Task.db traces ;
+      fail (Failure "Some workflow failed!")
     )
+    else
+      return (eval config.Task.db app)
   in
   Lwt_unix.run main
-
 
 type repo_item =
   Repo_item : string list * _ workflow -> repo_item
 
 let ( %> ) path w = Repo_item (path, w)
 
-let rec string_of_path = function
-  | [] -> "."
-  | "" :: t -> Filename.concat "." (string_of_path t)
-  | p -> List.reduce_exn p ~f:Filename.concat
-
 let make_absolute p =
   if Filename.is_absolute p then p
   else Filename.concat (Sys.getcwd ()) p
 
 let link p p_u =
-  let dst = string_of_path p in
+  let dst = Bistro.string_of_path p in
   let src = make_absolute p_u in
   Unix.mkdir_p (Filename.dirname dst) ;
   let cmd = sprintf "rm -rf %s && ln -s %s %s" dst src dst in
@@ -132,6 +109,16 @@ let link p p_u =
 
 let generate_page outdir (dest, cache_path) =
   link (outdir :: dest) cache_path
+
+let foreach_target { Task.db } outdir traces (Repo_item (dest, w)) =
+  let id = Bistro.Workflow.id w in
+  match String.Map.find_exn traces id with
+  | Tdag_sig.Run { outcome = Ok () }
+  | Tdag_sig.Skipped `Done_already ->
+    let cache_path = Db.cache db id in
+    link (outdir :: dest) cache_path
+  | Tdag_sig.Run { outcome = Error _ }
+  | Tdag_sig.Skipped `Missing_dep -> ()
 
 let of_repo ~outdir items =
   List.map items ~f:(function Repo_item (p, w) ->
--- lib/utils/bistro_app2.mli
+++ lib/utils/bistro_app2.mli
@@ -12,19 +12,14 @@ val ( $ ) : ('a -> 'b) t -> 'a t -> 'b t
 
 val list : 'a t list -> 'a list t
 
-
+val run :
+  ?use_docker:bool ->
+  ?np:int ->
+  ?mem:int ->
+  'a t -> 'a
 
 type repo_item
 
 val ( %> ) : string list -> _ workflow -> repo_item
 
 val of_repo : outdir:string -> repo_item list -> unit t
-
-
-
-val run :
-  ?use_docker:bool ->
-  ?np:int ->
-  ?mem:int ->
-  ?tmpdir:string ->
-  'a t -> 'a
