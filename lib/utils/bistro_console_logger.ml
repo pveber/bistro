a7127f0911c096c39f2659618dd4b1a261d1b7d4
Philippe Veber <philippe.veber@gmail.com>
2016-10-31 10:07:03 +0100

    utils/Console_logger: fixed termination bug
    call to `logger#stop` was ineffective because the thread could be
    blocker on the `new_event` condition.

--- lib/utils/bistro_console_logger.ml
+++ lib/utils/bistro_console_logger.ml
@@ -19,32 +19,34 @@ let error_short_descr =
     sprintf "ended with exit code %d" exit_code
 
 let rec loop stop queue new_event =
-  match Queue.dequeue queue with
-  | None ->
-    Lwt_condition.wait new_event >>= fun () ->
-    if !stop then Lwt.return ()
-    else loop stop queue new_event
-  | Some (t, ev) ->
-    Task.(
-      match ev with
-      | Scheduler.Task_started (Step s) ->
-        let id = String.prefix s.id 6 in
-        msg t "started %s.%s" s.descr id
+  if !stop then Lwt.return ()
+  else (
+    match Queue.dequeue queue with
+    | None ->
+      Lwt_condition.wait new_event >>= fun () ->
+      loop stop queue new_event
+    | Some (t, ev) ->
+      Task.(
+        match ev with
+        | Scheduler.Task_started (Step s) ->
+          let id = String.prefix s.id 6 in
+          msg t "started %s.%s" s.descr id
 
-      | Scheduler.Task_ended (Step s, res) ->
-        let id = String.prefix s.id 6 in
-        let outcome = match res with
-          | Ok () -> "success"
-          | Error e -> sprintf "error: %s" (error_short_descr e)
-        in
-        msg t "ended %s.%s (%s)" s.descr id outcome
+        | Scheduler.Task_ended (Step s, res) ->
+          let id = String.prefix s.id 6 in
+          let outcome = match res with
+            | Ok () -> "success"
+            | Error e -> sprintf "error: %s" (error_short_descr e)
+          in
+          msg t "ended %s.%s (%s)" s.descr id outcome
 
-      | Scheduler.Task_skipped (Step s, `Allocation_error err) ->
-        msg t "allocation error for %s.%s (%s)" s.descr s.id err
+        | Scheduler.Task_skipped (Step s, `Allocation_error err) ->
+          msg t "allocation error for %s.%s (%s)" s.descr s.id err
 
-      | _ -> Lwt.return ()
-    ) >>= fun () ->
-    loop stop queue new_event
+        | _ -> Lwt.return ()
+      ) >>= fun () ->
+      loop stop queue new_event
+  )
 
 class t =
   let queue = Queue.create () in
@@ -57,7 +59,8 @@ class t =
       Lwt_condition.signal new_event ()
 
     method stop =
-      stop := true
+      stop := true ;
+      Lwt_condition.signal new_event ()
 
     method wait4shutdown = loop
   end
