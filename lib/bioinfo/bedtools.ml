db2a0907ba03ab8b624d9589112dc95d02be555c
Philippe Veber <philippe.veber@gmail.com>
2016-11-03 14:22:33 +0100

    bioinfo/Bedtools: expose slop command and env

--- lib/bioinfo/bedtools.ml
+++ lib/bioinfo/bedtools.ml
@@ -12,25 +12,30 @@ type 'a input = Bed | Gff
 let bed = Bed
 let gff = Gff
 
-let slop_args ?strand ?header ~mode = [
-  option (flag string "-s") strand ;
-  option (flag string "-header") header ;
-  seq (
-    match mode with
-    | `both n      -> [ opt "-b" int n ]
-    | `left n      -> [ opt "-l" int n ]
-    | `right n     -> [ opt "-r" int n ]
-    | `left_pct p  -> [ opt "-l" float p ; string "-pct" ]
-    | `right_pct p -> [ opt "-l" float p ; string "-pct" ]
-    | `both_pct p  -> [ opt "-b" float p ; string "-pct" ]
-  )
-]
+module Cmd = struct
+  let slop_args ?strand ?header ~mode = [
+    option (flag string "-s") strand ;
+    option (flag string "-header") header ;
+    seq (
+      match mode with
+      | `both n      -> [ opt "-b" int n ]
+      | `left n      -> [ opt "-l" int n ]
+      | `right n     -> [ opt "-r" int n ]
+      | `left_pct p  -> [ opt "-l" float p ; string "-pct" ]
+      | `right_pct p -> [ opt "-l" float p ; string "-pct" ]
+      | `both_pct p  -> [ opt "-b" float p ; string "-pct" ]
+    )
+  ]
 
-let slop ?strand ?header ~mode format input chrom_size =
-  workflow ~descr:"bedtools.slop" [
+  let slop ?strand ?header ~mode format input chrom_size =
     bedtools "slop" [
       seq (slop_args ?strand ?header ~mode) ;
       opt "-i" dep input ;
       opt "-g" dep chrom_size ;
     ]
+end
+
+let slop ?strand ?header ~mode format input chrom_size =
+  workflow ~descr:"bedtools.slop" [
+    Cmd.slop ?strand ?header ~mode format input chrom_size
   ]
--- lib/bioinfo/bedtools.mli
+++ lib/bioinfo/bedtools.mli
@@ -1,10 +1,31 @@
 open Bistro.Std
 open Defs
 
+val env : Bistro.docker_image
+
 type 'a input
 
 val bed : #bed3 input
 val gff : gff input
+
+module Cmd : sig
+  val slop :
+    ?strand:bool ->
+    ?header:bool ->
+    mode:[
+      | `both of int
+      | `left of int
+      | `right of int
+      | `both_pct of float
+      | `left_pct of float
+      | `right_pct of float
+    ] ->
+    'a input ->
+    'a workflow ->
+    Ucsc_gb.chrom_sizes workflow ->
+    Bistro.command
+end
+
 
 val slop :
   ?strand:bool ->
