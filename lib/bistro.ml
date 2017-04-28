pveber/bistro

commit 2d74e590a43ac81ef3a8c3d6064b813374920171

engine/Scheduler: fixed performance bug in workflow compilation
compilation to dag was particularly long when passed a big number of
workflows. This was due to precious workflow computation not keeping
enough information along the calculation (already visited workflows).

lib/engine/scheduler.ml
@@ -104,11 +104,11 @@ let precious_workflows acc w =
       (S.add seen s.id,
        if s.precious then S.add precious_deps s.id else precious_deps)
   in
-  traverse (String.Set.empty, acc) w
-  |> snd
+  traverse acc w
 
 let precious_workflows_of_list xs =
-  List.fold xs ~init:String.Set.empty ~f:precious_workflows
+  List.fold xs ~init:(String.Set.empty, String.Set.empty) ~f:precious_workflows
+  |> snd
 
 let compile workflows =
   let workflows = List.map workflows ~f:(fun (Bistro.Workflow w) -> Bistro.Workflow.u w) in

commit 76845717a7f43afaa9b7359386519464f66b0cac

API change on defining precious workflows
Defining precious worklows via the previously introduced
[EDSL.precious] combinator was abandonned because:
- particular workflows could be sometimes flagged with precious
  attributes and sometimes not, which prompted for dedicated
  traversals of workflow values, to see if at least one copy in memory
  had the flag. This was costly because one could not count on
  workflow ids to ensure a workflow had been visited (precious
  attribute did not modify id). In this regard, commit 2d74e59
  introduced a bug trying to make the process faster. In case of
  workflows with many sharing, this could lead to exponential
  complexity.
- it was not really clean conceptually, since precious attribute was
  about what to do with the workflow in case in it is an
  intermediate. This feels more scheduler-related.

This commit settles for a simpler (although a bit less practical)
design, where precious workflows are passed directly to the
scheduler.

lib/bistro.ml
@@ -100,7 +100,6 @@ module T = struct
     np : int ; (** Required number of processors *)
     mem : int ; (** Required memory in MB *)
     version : int option ; (** Version number of the wrapper *)
-    precious : bool ;
   }
 
   and action =
@@ -267,11 +266,10 @@ module Workflow = struct
       ?(mem = 100)
       ?(np = 1)
       ?version
-      ?(precious = false)
       action =
     let deps = deps_of_action action in
     let id = digest ("step", version, digestable_action action) in
-    Step { descr ; deps ; action ; np ; mem ; version ; id ; precious }
+    Step { descr ; deps ; action ; np ; mem ; version ; id }
 
   let select u (Selector path) =
     let u, path =
@@ -440,10 +438,6 @@ module EDSL = struct
     dck_registry = registry ;
   }
 
-  let rec precious = function
-    | (Input _ as w) -> w
-    | Select (id, u, p) -> Select (id, precious u, p)
-    | Step s -> Step { s with precious = true }
 end
 
 module EDSL' = struct

lib/bistro.mli
@@ -77,7 +77,6 @@ and step = private {
   np : int ; (** Required number of processors *)
   mem : int ; (** Required memory in MB *)
   version : int option ; (** Version number of the wrapper *)
-  precious : bool ;
 }
 
 and action =
@@ -226,13 +225,6 @@ module EDSL : sig
   val ( / ) : 'a directory workflow -> ('a, 'b) selector -> 'b workflow
   (** Constructs a workflow by selecting a dir or file from a
       directory workflow *)
-
-  val precious : 'a workflow -> 'a workflow
-  (** [precious w] is [w] plus its [precious] attribute set to
-      [true]. This is to tell an execution engine that the target
-      takes a long time to be generated and should be kept in cache
-      whenever possible.*)
-
 
   val cmd :
     string ->

lib/engine/scheduler.ml
@@ -29,20 +29,28 @@ let workflow_id =
   | Select (id, _, _)
   | Step { id } -> id
 
-let task_of_workflow precious w =
-  let open Task in
-  match of_workflow w with
-  | Step s as t ->
-    if String.Set.mem precious s.id
-    then Step { s with precious = true }
-    else t
-  | t -> t
 
-let rec add_workflow precious (seen, dag) w =
+(* If [w] is a select, we need to ensure its parent dir is
+   marked as precious. [w] only performs a side effect, the
+   real contents is in the result of selected workflow. *)
+let precious_expand =
+  let open Bistro in
+  function
+  | (Input _ as w) -> [ w ]
+  | Select (_, (Step _ as s), _) as w ->
+    [ w ; s ]
+  | Select (_, (Input _ | Select _), _) as w -> [ w ]
+  | Step _ as w -> [ w ]
+
+let precious_expansion = List.concat_map ~f:precious_expand
+
+
+let rec add_workflow precious_set (seen, dag) w =
   let id = workflow_id w in
   match String.Map.find seen id with
   | None ->
-    let u = task_of_workflow precious w in
+    let precious = String.Set.mem precious_set id in
+    let u = Task.of_workflow ~precious w in
     let seen', dag' =
       List.fold (workflow_deps w) ~init:(seen, DAG.add_task dag u) ~f:(fun accu dep ->
           (* If [dep] is a select, we need to add its parent dir as a
@@ -52,12 +60,12 @@ let rec add_workflow precious (seen, dag) w =
           let accu = Bistro.(
               match dep with
               | Select (_, dep_dir, _) ->
-                let seen, dag, dep_dir_v = add_workflow precious accu dep_dir in
+                let seen, dag, dep_dir_v = add_workflow precious_set accu dep_dir in
                 seen, DAG.add_dep dag u ~on:dep_dir_v
               | Input _ | Step _ -> accu
             )
           in
-          let seen, dag, dep_v = add_workflow precious accu dep in
+          let seen, dag, dep_v = add_workflow precious_set accu dep in
           String.Map.add seen id u,
           DAG.add_dep dag u ~on:dep_v
         )
@@ -67,28 +75,15 @@ let rec add_workflow precious (seen, dag) w =
   | Some u -> seen, dag, u
 
 
-let precious_workflows acc w =
-  let open Bistro in
-  let module S = String.Set in
-  let rec traverse acc = function
-    | Input _ -> acc
-    | Select (_, dir, _) -> traverse acc dir
-    | Step s ->
-      if S.mem (fst acc) s.id then acc
-      else
-      let (seen, precious_deps) = List.fold s.deps ~init:acc ~f:traverse in
-      (S.add seen s.id,
-       if s.precious then S.add precious_deps s.id else precious_deps)
-  in
-  traverse acc w
-
-let precious_workflows_of_list xs =
-  List.fold xs ~init:(String.Set.empty, String.Set.empty) ~f:precious_workflows
-  |> snd
-
-let compile workflows =
+let compile ?(precious = []) workflows =
   let workflows = List.map workflows ~f:(fun (Bistro.Workflow w) -> Bistro.Workflow.u w) in
-  let precious_workflows = precious_workflows_of_list workflows in
+  let precious_workflows =
+    precious
+    |> Bistro.(List.map ~f:(fun (Workflow w) -> Workflow.u w))
+    |> precious_expansion
+    |> List.map ~f:Bistro.Workflow.id'
+    |> String.Set.of_list
+  in
   workflows
   |> List.fold ~init:(String.Map.empty, DAG.empty, []) ~f:(fun (seen, dag, goals) u ->
       let seen, dag, t = add_workflow precious_workflows (seen, dag) u in

lib/engine/scheduler.mli
@@ -32,7 +32,13 @@ type trace =
                | `Missing_dep
                | `Allocation_error of string ]
 
-val compile : Bistro.any_workflow list -> DAG.t * Task.t list
+(* Parameter [precious] is used to tell an execution engine that the
+   target takes a long time to be generated and should be kept in
+   cache whenever possible.*)
+val compile :
+  ?precious:Bistro.any_workflow list ->
+  Bistro.any_workflow list ->
+  DAG.t * Task.t list
 
 val run :
   ?logger:logger ->

lib/engine/task.ml
@@ -192,7 +192,7 @@ let denormalize_action = function
   | Bistro.Eval (Bistro.File expr) -> Eval (File (denormalize_expression expr))
   | Bistro.Eval (Bistro.Directory expr) -> Eval (Directory (denormalize_expression expr))
 
-let of_step { Bistro.id ; mem ; np ; descr ; action ; deps ; version ; precious } =
+let of_step ~precious { Bistro.id ; mem ; np ; descr ; action ; deps ; version } =
   Step {
     id ;
     descr ;
@@ -204,14 +204,14 @@ let of_step { Bistro.id ; mem ; np ; descr ; action ; deps ; version ; precious 
     precious ;
   }
 
-let of_workflow = function
+let of_workflow ~precious = function
   | Bistro.Input (id, p) -> Input (id, p)
   | Bistro.Select (id, Bistro.Step { Bistro.id = dir_id }, p) ->
     Select (id, `Step dir_id, p)
   | Bistro.Select (id, Bistro.Input (_, dir_p), p) ->
     Select (id, `Input dir_p, p)
   | Bistro.Select (_, Bistro.Select _, _) -> assert false
-  | Bistro.Step s -> of_step s
+  | Bistro.Step s -> of_step ~precious s
 
 let requirement = function
   | Input _

lib/engine/task.mli
@@ -1,3 +1,5 @@
+open Core.Std
+
 type t =
   | Input of string * path
   | Select of string * [`Input of path | `Step of string] * path
@@ -85,7 +87,7 @@ val config :
   keep_all:bool ->
   config
 
-val of_workflow : Bistro.u -> t
+val of_workflow : precious:bool -> Bistro.u -> t
 val id : t -> string
 val requirement : t -> Allocator.request
 val perform : Allocator.resource -> config -> t -> result Lwt.t

lib/utils/bistro_app.ml
@@ -49,11 +49,7 @@ let rec to_workflow_list term =
       | Pure _ -> ()
       | PureW w ->
         let open Bistro in
-        (* If [w] is a select, we need to ensure its parent dir is
-           marked as precious. [w] only performs a side effect, the
-           real contents is in the result of selected workflow. This
-           is done by the inner logic of [EDSL.precious] *)
-        Hash_set.add acc (Workflow (EDSL.precious w))
+        Hash_set.add acc (Workflow w)
       | App (f, x) ->
         aux f ; aux x
       | List xs ->

lib/utils/bistro_repo.ml
@@ -86,13 +86,17 @@ let generate outdir items =
       link file_path cache_path
     )
 
-let to_app ~outdir items =
+let use t (Bistro.Workflow w) =
+  pure (fun x _ -> x) $ t $ pureW w
+
+let to_app ?(precious = []) ~outdir items =
   List.map items ~f:(function (Repo_item (p, w) as item) ->
       pure (normalized_repo_item item) $ (pureW w)
     )
   |> list
   |> app (pure remove_redundancies)
   |> app (pure (generate outdir))
+  |> fun init -> List.fold precious ~init ~f:use
 
-let build ?np ?mem ?logger ?keep_all ~outdir repo =
-  Bistro_app.run ?np ?mem ?logger ?keep_all (to_app ~outdir repo)
+let build ?np ?mem ?logger ?keep_all ?precious ~outdir repo =
+  Bistro_app.run ?np ?mem ?logger ?keep_all (to_app ~outdir ?precious repo)

lib/utils/bistro_repo.mli
@@ -7,11 +7,16 @@ type t = item list
 
 val ( %> ) : string list -> _ workflow -> item
 
-val to_app : outdir:string -> t -> unit Bistro_app.t
+val to_app :
+  ?precious:Bistro.any_workflow list ->
+  outdir:string ->
+  t ->
+  unit Bistro_app.t
 
 val build  :
   ?np:int ->
   ?mem:int ->
   ?logger:Scheduler.logger ->
   ?keep_all:bool ->
+  ?precious:Bistro.any_workflow list ->
   outdir:string -> t -> unit