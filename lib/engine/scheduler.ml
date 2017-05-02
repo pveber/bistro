pveber/bistro

commit 13d3f9ee4eddbd14bac78e44abeabc1b5ceb6e41

engine/Scheduler: bug fix and API simplification (precious stuff again)
following recent changes on precious workflows, the very goals of an
App were not even marked precious, and were thus deleted at the
end. Not cool. This should be fixed, and the API was simplified:
compile doesn't output the goal tasks of the DAG. That wasn't used
anyway.

lib/engine/scheduler.ml
@@ -75,21 +75,22 @@ let rec add_workflow precious_set (seen, dag) w =
   | Some u -> seen, dag, u
 
 
-let compile ?(precious = []) workflows =
-  let workflows = List.map workflows ~f:(fun (Bistro.Workflow w) -> Bistro.Workflow.u w) in
-  let precious_workflows =
-    precious
-    |> Bistro.(List.map ~f:(fun (Workflow w) -> Workflow.u w))
+let compile workflows =
+  let workflows =
+    List.map workflows ~f:(fun (Bistro.Workflow w) -> Bistro.Workflow.u w)
+  in
+  let precious_ids =
+    workflows
     |> precious_expansion
     |> List.map ~f:Bistro.Workflow.id'
     |> String.Set.of_list
   in
   workflows
-  |> List.fold ~init:(String.Map.empty, DAG.empty, []) ~f:(fun (seen, dag, goals) u ->
-      let seen, dag, t = add_workflow precious_workflows (seen, dag) u in
-      seen, dag, t :: goals
+  |> List.fold ~init:(String.Map.empty, DAG.empty) ~f:(fun (seen, dag) u ->
+      let seen, dag, _ = add_workflow precious_ids (seen, dag) u in
+      seen, dag
     )
-  |> fun (_, dag, goals) -> dag, goals
+  |> snd
 
-let run ?logger ?goals alloc config dag =
-  DAG.run ?logger ?goals alloc config dag
+let run ?logger alloc config dag =
+  DAG.run ?logger alloc config dag

lib/engine/scheduler.mli
@@ -32,17 +32,12 @@ type trace =
                | `Missing_dep
                | `Allocation_error of string ]
 
-(* Parameter [precious] is used to tell an execution engine that the
-   target takes a long time to be generated and should be kept in
-   cache whenever possible.*)
 val compile :
-  ?precious:Bistro.any_workflow list ->
   Bistro.any_workflow list ->
-  DAG.t * Task.t list
+  DAG.t
 
 val run :
   ?logger:logger ->
-  ?goals:Task.t list ->
   Task.config ->
   Allocator.t ->
   DAG.t ->

lib/utils/bistro_app.ml
@@ -162,8 +162,8 @@ let run
     let config = Task.config ~db_path:"_bistro" ~use_docker:true ~keep_all in
     let allocator = Allocator.create ~np ~mem in
     let workflows = to_workflow_list app in
-    let dag, goals = Scheduler.compile workflows in
-    Scheduler.(run ?logger ~goals config allocator dag) >>= fun traces ->
+    let dag = Scheduler.compile workflows in
+    Scheduler.(run ?logger config allocator dag) >>= fun traces ->
     (
       match logger with
       | Some logger ->

commit 33feb37e694402a1e31ebb0184d6af90d657b2fc

fixed bug introduced in 13d3f9e

lib/engine/scheduler.ml
@@ -85,12 +85,13 @@ let compile workflows =
     |> List.map ~f:Bistro.Workflow.id'
     |> String.Set.of_list
   in
-  workflows
-  |> List.fold ~init:(String.Map.empty, DAG.empty) ~f:(fun (seen, dag) u ->
-      let seen, dag, _ = add_workflow precious_ids (seen, dag) u in
-      seen, dag
+  let _, dag, goals =
+    List.fold workflows ~init:(String.Map.empty, DAG.empty, []) ~f:(fun (seen, dag, goals) u ->
+        let seen, dag, t = add_workflow precious_ids (seen, dag) u in
+        seen, dag, t :: goals
     )
-  |> snd
+  in
+  dag, goals
 
-let run ?logger alloc config dag =
-  DAG.run ?logger alloc config dag
+let run ?logger ?goals alloc config dag =
+  DAG.run ?logger ?goals alloc config dag

lib/engine/scheduler.mli
@@ -34,10 +34,11 @@ type trace =
 
 val compile :
   Bistro.any_workflow list ->
-  DAG.t
+  DAG.t * Task.t list
 
 val run :
   ?logger:logger ->
+  ?goals:Task.t list ->
   Task.config ->
   Allocator.t ->
   DAG.t ->

lib/utils/bistro_app.ml
@@ -162,8 +162,8 @@ let run
     let config = Task.config ~db_path:"_bistro" ~use_docker:true ~keep_all in
     let allocator = Allocator.create ~np ~mem in
     let workflows = to_workflow_list app in
-    let dag = Scheduler.compile workflows in
-    Scheduler.(run ?logger config allocator dag) >>= fun traces ->
+    let dag, goals = Scheduler.compile workflows in
+    Scheduler.(run ?logger ~goals config allocator dag) >>= fun traces ->
     (
       match logger with
       | Some logger ->