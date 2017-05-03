d5954cfbe1b8bcb6289663a60ddbd61228b7f80d
Philippe Veber <philippe.veber@gmail.com>
2017-05-03 14:32:41 +0200

    bioinfo: new module for ChIPQC

---
+++ lib/bioinfo/chIPQC.mli
@@ -0,0 +1,16 @@
+open Bistro.Std
+open Bistro.EDSL
+open Defs
+
+type 'a sample = {
+  id : string ;
+  tissue : string ;
+  factor : string ;
+  replicate : string ;
+  bam : bam workflow ;
+  peaks : (#bed3 as 'a) workflow ;
+}
+
+type output = [`ChIPQC] directory
+
+val run : 'a sample list -> output workflow
