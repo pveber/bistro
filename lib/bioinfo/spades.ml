f1785aa677e7491ef5196316ed842a61b224defe
Philippe Veber <philippe.veber@gmail.com>
2017-06-06 12:56:40 +0200

    bioinfo: added wrapper for Spades

---
+++ lib/bioinfo/spades.ml
@@ -0,0 +1,36 @@
+open Core_kernel.Std
+open Bistro.Std
+open Bistro.EDSL
+
+let pe_args (ones, twos) =
+  let opt side i x =
+    opt (sprintf "--pe%d-%d" (i + 1) side) dep x
+  in
+  seq ~sep:" " (
+    List.mapi ones ~f:(opt 1)
+    @
+    List.mapi twos ~f:(opt 2)
+  )
+
+let spades
+    ?single_cell ?iontorrent
+    ?pe
+    ?(threads = 4)
+    ?(memory = 10)
+    ()
+  : [`spades] directory workflow
+  =
+  workflow ~np:threads ~mem:(memory * 1024) ~descr:"spades" [
+    mkdir_p dest ;
+    cmd "spades.py" [
+      option (flag string "--sc") single_cell ;
+      option (flag string "--iontorrent") iontorrent ;
+      opt "--threads" ident np ;
+      opt "--memory" (fun m -> seq [ string "$((" ; mem ; string " / 1024))" ]) mem ;
+      option pe_args pe ;
+      opt "-o" ident dest ;
+    ]
+  ]
+
+let contigs = selector ["contigs.fasta"]
+let scaffolds = selector ["scaffolds.fasta"]
