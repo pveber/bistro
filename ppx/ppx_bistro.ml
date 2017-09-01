b248217a7271395934ffc0965d584285cab72106
Philippe Veber <philippe.veber@gmail.com>
2017-09-01 18:45:05 +0200

    ppx_bistro: deps extraction, generation of id and actual function

--- ppx/ppx_bistro.ml
+++ ppx/ppx_bistro.ml
@@ -1,3 +1,4 @@
+open Base
 open Migrate_parsetree
 open Ast_404
 open Ast_mapper
@@ -5,14 +6,70 @@ open Asttypes
 open Parsetree
 open Ast_helper
 
+let lid ?(loc= !default_loc) str =
+  Location.mkloc (Longident.parse str) loc
+
+let str ?(loc= !default_loc) str =
+  Location.mkloc str loc
+
+module Exp = struct
+  include Exp
+  let string s = constant (Const.string s)
+  let lid ?loc s = ident (lid ?loc s)
+end
+
+let ( += ) r v = r := v :: !r
+
+let digest x =
+  Caml.Digest.to_hex (Caml.Digest.string (Caml.Marshal.to_string x []))
+
 let ocaml_version = Versions.ocaml_404
+
+let new_id =
+  let c = ref 0 in
+  fun () -> "v" ^ (Int.to_string !c)
+
+let payload_rewriter acc =
+  { default_mapper with
+    expr = fun mapper expr ->
+      match expr with
+      | { pexp_desc = Pexp_extension ({txt = "dep"}, payload) ; pexp_loc = loc } -> (
+          match payload with
+          | PStr [ { pstr_desc = Pstr_eval (e, _) ; pstr_loc = loc } ] ->
+            let id = new_id () in
+            acc += (id, e) ;
+            [%expr env#dep (`Dep [%e Exp.lid (id ^ "_id")])]
+          | _ -> failwith "expected a workflow expression"
+        )
+      | _ -> default_mapper.expr mapper expr
+  }
 
 let rewriter _config _cookies =
   { default_mapper with
     expr = fun mapper expr ->
       match expr with
-      | { pexp_desc = Pexp_extension ({txt = "bistro_fun"}, payload) } ->
-        Exp.constant (Const.int 42)
+      | { pexp_desc = Pexp_extension ({txt = "bistro_fun"}, payload) } -> (
+          match payload with
+          | PStr [ { pstr_desc = Pstr_eval (expr, _) ; pstr_loc = loc } ] ->
+            let deps = ref [] in
+            let rewriter = payload_rewriter deps in
+            let code = rewriter.expr rewriter expr in
+            let id = digest code in
+            let add_bindings body = List.fold !deps ~init:body ~f:(fun acc (tmpvar, expr) ->
+                [%expr
+                  let [%p Pat.var (str tmpvar)] = [%e expr] in
+                  let [%p Pat.var (str (tmpvar ^ "_id"))] = Bistro.Workflow.id [%e Exp.lid tmpvar] in
+                  [%e acc]]
+              )
+            in
+            [%expr
+              let id = [%e Exp.string id] in
+              let f env = [%e code] in
+              (id, f) ]
+            |> add_bindings
+          | _ -> failwith "bistro_fun extension expects a single expression as payload"
+        )
+
       | _ -> default_mapper.expr mapper expr
   }
 
@@ -22,6 +79,16 @@ let () =
     ocaml_version
     rewriter
 
+
+(* open Ppx_core *)
+(* open Ast_builder.Default *)
+
+(* let transformer ~loc ~path expr attrs = *)
+(*   eint ~loc 42 *)
+
+(* let _ = *)
+(*   let ast_pattern = Ast_pattern.(pstr ((pstr_eval __ __) ^:: nil)) in *)
+(*   Extension.declare "bistro_fun" Extension.Context.expression ast_pattern transformer *)
 
 (* open Ast_mapper *)
 (* (\* open Ast_helper *\) *)
