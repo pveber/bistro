584e5c608fa6ae4a1d6128b4befe3a0ec386c73c
Philippe Veber <philippe.veber@gmail.com>
2017-10-03 16:32:28 +0200

    bioinfo/Spades: fixed wrapper
    ... because spades insists on reading file extensions

--- lib/bioinfo/spades.ml
+++ lib/bioinfo/spades.ml
@@ -4,15 +4,18 @@ open Bistro.EDSL
 
 let env = docker_image ~account:"pveber" ~name:"spades" ~tag:"3.9.1" ()
 
-let pe_args (ones, twos) =
-  let opt side i x =
-    opt (sprintf "--pe%d-%d" (i + 1) side) dep x
+let renamings (ones, twos) =
+  let f side i x =
+    let id = sprintf "pe%d-%d" (i + 1) side in
+    let new_name = seq ~sep:"/" [ tmp ; string (id ^ ".fq") ] in
+    let opt = opt (sprintf "--pe%d-%d" (i + 1) side) ident new_name in
+    let cmd = cmd "ln" [ string "-s" ; dep x ; new_name ] in
+    opt, cmd
   in
-  seq ~sep:" " (
-    List.mapi ones ~f:(opt 1)
-    @
-    List.mapi twos ~f:(opt 2)
-  )
+  let r = List.mapi ones ~f:(f 1) @ List.mapi twos ~f:(f 2) in
+  let args = seq ~sep:" " (List.map r ~f:fst) in
+  let cmds = List.map r ~f:snd in
+  Some args, cmds
 
 let spades
     ?single_cell ?iontorrent
@@ -22,16 +25,27 @@ let spades
     ()
   : [`spades] directory workflow
   =
+  let pe_args, ln_commands = match pe with
+    | None -> None, []
+    | Some files -> renamings files
+  in
   workflow ~np:threads ~mem:(memory * 1024) ~descr:"spades" [
+    mkdir_p tmp ;
     mkdir_p dest ;
-    cmd "spades.py" ~env [
-      option (flag string "--sc") single_cell ;
-      option (flag string "--iontorrent") iontorrent ;
-      opt "--threads" ident np ;
-      opt "--memory" (fun m -> seq [ string "$((" ; mem ; string " / 1024))" ]) mem ;
-      option pe_args pe ;
-      opt "-o" ident dest ;
-    ]
+    docker env (
+      and_list (
+        ln_commands @ [
+          cmd "spades.py" ~env [
+            option (flag string "--sc") single_cell ;
+            option (flag string "--iontorrent") iontorrent ;
+            opt "--threads" ident np ;
+            opt "--memory" (fun m -> seq [ string "$((" ; mem ; string " / 1024))" ]) mem ;
+            option ident pe_args ;
+            opt "-o" ident dest ;
+          ]
+        ]
+      )
+    )
   ]
 
 let contigs = selector ["contigs.fasta"]
