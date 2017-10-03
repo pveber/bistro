pveber/bistro

commit 18730e0dcb692443630a85ebfae089629f67dac2

utils: memory specification is now given in GB

lib/utils/bistro_app.ml
@@ -158,7 +158,7 @@ let has_error traces =
     )
 
 let create
-    ?(np = 1) ?(mem = 1024) ?logger ?(keep_all = true)
+    ?(np = 1) ?mem:(`GB mem = `GB 1) ?logger ?(keep_all = true)
     ?(bistro_dir = "_bistro")
     app
   =

lib/utils/bistro_app.mli
@@ -21,7 +21,7 @@ val assoc : ('a * 'b t) list -> ('a * 'b) list t
 
 val create :
   ?np:int ->
-  ?mem:int ->
+  ?mem:[`GB of int] ->
   ?logger:Scheduler.logger ->
   ?keep_all:bool ->
   ?bistro_dir:string ->
@@ -29,7 +29,7 @@ val create :
 
 val run :
   ?np:int ->
-  ?mem:int ->
+  ?mem:[`GB of int] ->
   ?logger:Scheduler.logger ->
   ?keep_all:bool ->
   ?bistro_dir:string ->

lib/utils/bistro_repo.mli
@@ -15,7 +15,7 @@ val to_app :
 
 val build  :
   ?np:int ->
-  ?mem:int ->
+  ?mem:[`GB of int] ->
   ?logger:Scheduler.logger ->
   ?keep_all:bool ->
   ?precious:Bistro.any_workflow list ->

commit b2438ed571fb203a26ab8637db81fbb9ed0de3f8

fixed bug introduced in 18730e0

lib/utils/bistro_app.ml
@@ -163,7 +163,7 @@ let create
     app
   =
   let open Lwt in
-  let allocator = Allocator.create ~np ~mem in
+  let allocator = Allocator.create ~np ~mem:(mem * 1024) in
   let workflows = to_workflow_list app in
   let dag, goals, precious = Scheduler.compile workflows in
   let config = Task.config ~db_path:bistro_dir ~use_docker:true ~keep_all ~precious in