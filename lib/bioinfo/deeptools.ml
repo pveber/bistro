8ecaa2319da9524037bb8f2688d3234814294e72
Votre Nom <Vous@exemple.com>
2017-10-10 20:14:06 +0200

    bioinfo/Deeptools: added heatmap plot

--- lib/bioinfo/deeptools.ml
+++ lib/bioinfo/deeptools.ml
@@ -4,7 +4,8 @@ open Bistro.EDSL
 
 let env = docker_image ~account:"pveber" ~name:"deeptools" ~tag:"2.4.1" ()
 
-type 'a output = [ `bigWig | `bedGraph ]
+type 'a signal_format = [ `bigWig | `bedGraph ]
+type 'a img_format = [ `png | `pdf | ` svg ]
 
 class type compressed_numpy_array = object
   inherit binary_file
@@ -13,6 +14,14 @@ end
 
 let bigwig = `bigWig
 let bedgraph = `bedGraph
+let png = `png
+let pdf = `png
+let svg = `svg
+
+let ext_of_format = function
+  | `png -> "png"
+  | `pdf -> "pdf"
+  | `svg -> "svg"
 
 let file_format_expr = function
   | `bigWig -> string "bigwig"
@@ -36,6 +45,9 @@ let ratio_expr = function
   | `first -> string "first"
   | `second -> string "second"
 
+let slist f x = list ~sep:" " f x
+
+let dep_list xs = slist dep xs
 
 (*Transform a number from French format into an US format.*)
 let normalizeto1x_expr nbr =
@@ -195,3 +207,181 @@ let multibamsummary_bed ?region ?blacklistfilename ?(threads = 1)
       opt "--outFileName" ident dest ;
     ]
   ]
+
+let reference_point_enum =
+  Fn.compose string (function
+      | `TES -> "TES"
+      | `TSS -> "TSS"
+      | `center -> "center"
+    )
+
+let sort_regions_enum x =
+  Fn.compose string (function
+      | `no -> "no"
+      | `ascend -> "ascend"
+      | `descend -> "descend"
+      | `keep -> "keep"
+    ) x
+
+let sort_using_enum =
+  Fn.compose string (function
+      | `max -> "max"
+      | `min -> "min"
+      | `mean -> "mean"
+      | `median -> "median"
+      | `region_length -> "region_length"
+      | `sum -> "sum"
+    )
+
+let average_type_bins_enum =
+  Fn.compose string (function
+      | `max -> "max"
+      | `min -> "min"
+      | `mean -> "mean"
+      | `median -> "median"
+      | `std -> "std"
+      | `sum -> "sum"
+    )
+
+let what_to_show_enum =
+  Fn.compose string (function
+      | `plot_heatmap_and_colorbar -> "plot, heatmap and colorbar"
+      | `plot_and_heatmap -> "plot and heatmap"
+      | `heatmap_only -> "heatmap only"
+      | `heatmap_and_colorbar -> "heatmap and colorbar"
+    )
+
+
+let legend_location_enum =
+  Fn.compose string (function
+      | `best-> "best"
+      | `upper_right-> "upper_right"
+      | `upper_left-> "upper_left"
+      | `upper_center-> "upper_center"
+      | `lower_left-> "lower_left"
+      | `lower_right-> "lower_right"
+      | `lower_center-> "lower_center"
+      | `center-> "center"
+      | `center_left-> "center_left"
+      | `center_right-> "center_right"
+      | `none -> "none"
+    )
+
+
+
+class type deeptools_matrix = object
+  inherit binary_file
+  method format : [`deeptools_matrix]
+end
+
+let computeMatrix_reference_point
+    ?referencePoint ?upstream ?downstream ?nanAfterEnd
+    ?binSize ?sortRegions ?sortUsing ?sortUsingSamples
+    ?averageTypeBins ?missingDataAsZero ?skipZeros
+    ?minThreshold ?maxThreshold ?blackList ?scale
+    ?(numberOfProcessors = 1) ~regions ~scores () =
+  workflow ~descr:"deeptools.computeMatrix_reference_point" ~np:numberOfProcessors [
+    cmd "computeMatrix" ~env [
+      string "reference-point" ;
+      option (opt "--referencePoint" reference_point_enum) referencePoint ;
+      option (opt "--upstream" int) upstream ;
+      option (opt "--downstream" int) downstream ;
+      option (flag string "--nanAfterEnd") nanAfterEnd ;
+      option (opt "--binSize" int) binSize ;
+      option (opt "--sortRegions" sort_regions_enum) sortRegions ;
+      option (opt "--sortUsing" sort_using_enum) sortUsing ;
+      option (opt "--sortUsingSamples" (slist int) ) sortUsingSamples ;
+      option (opt "--averageTypeBins" average_type_bins_enum) averageTypeBins ;
+      option (flag string "--missingDataAsZero") missingDataAsZero ;
+      option (flag string "--skipZeros") skipZeros ;
+      option (opt "--minThreshold" float) minThreshold ;
+      option (opt "--maxThreshold" float) maxThreshold ;
+      option (opt "--blackListFileName" dep) blackList ;
+      option (opt "--sc" float) scale ;
+      opt "--outFileName" Fn.id dest ;
+      opt "--regionsFileName" dep_list regions ;
+      opt "--scoreFileName" dep_list scores ;
+    ]
+  ]
+
+let plotHeatmap
+  ?dpi ?kmeans ?hclust ?sortRegions ?sortUsing ?sortUsingSamples
+  ?averageTypeSummaryPlot ?missingDataColor ?colorMap ?alpha
+  ?colorList ?colorNumber ?zMin ?zMax ?heatmapHeight ?heatmapWidth
+  ?whatToShow ?boxAroundHeatmaps ?xAxisLabel ?startLabel
+  ?endLabel ?refPointLabel ?regionsLabel ?samplesLabel ?plotTitle
+  ?yAxisLabel ?yMin ?yMax ?legendLocation ?perGroup
+  output_format matrix =
+  let tmp_file = tmp // ("file." ^ ext_of_format output_format) in
+  workflow ~descr:"deeptools.plotHeatmap" [
+    cmd "plotHeatmap" ~env [
+      option (opt "--dpi" int) dpi ;
+      option (opt "--kmeans" int) kmeans ;
+      option (opt "--hclust" int) hclust ;
+      option (opt "--sortRegions" sort_regions_enum) sortRegions ;
+      option (opt "--sortUsing" sort_using_enum) sortUsing ;
+      option (opt "--sortUsingSamples" (slist int)) sortUsingSamples ;
+      option (opt "--averageTypeSummaryPlot" average_type_bins_enum) averageTypeSummaryPlot ;
+      option (opt "--missingDataColor" string) missingDataColor ;
+      option (opt "--colorMap" string) colorMap ;
+      option (opt "--alpha" float) alpha ;
+      option (opt "--colorList" (slist string)) colorList ;
+      option (opt "--colorNumber" int) colorNumber ;
+      option (opt "--zMin" (slist float)) zMin ;
+      option (opt "--zMax" (slist float)) zMax ;
+      option (opt "--heatmapHeight" float) heatmapHeight ;
+      option (opt "--heatmapWidth" float) heatmapWidth ;
+      option (opt "--whatToShow" what_to_show_enum) whatToShow ;
+      option (opt "--boxAroundHeatmaps" (fun b -> string (if b then "yes" else "no"))) boxAroundHeatmaps ;
+      option (opt "--xAxisLabel" string) xAxisLabel ;
+      option (opt "--startLabel" string) startLabel  ;
+      option (opt "--endLabel" string) endLabel  ;
+      option (opt "--refPointLabel" string) refPointLabel  ;
+      option (opt "--regionsLabel" (slist string)) regionsLabel ;
+      option (opt "--samplesLabel" (slist string)) samplesLabel ;
+      option (opt "--plotTitle" string) plotTitle ;
+      option (opt "--yAxisLabel" string) yAxisLabel ;
+      option (opt "--yMin" (slist float)) yMin ;
+      option (opt "--yMax" (slist float)) yMax ;
+      option (opt "--legendLocation" legend_location_enum) legendLocation ;
+      option (flag string "--perGroup") perGroup ;
+      opt "--matrixFile" dep matrix ;
+      opt "--outFileName" Fn.id tmp_file ;
+    ] ;
+    mv tmp_file dest ;
+  ]
+
+let plotHeatmap = (plotHeatmap :   ?dpi:int ->
+  ?kmeans:int ->
+  ?hclust:int ->
+  ?sortRegions:[`descend | `ascend | `no] ->
+  ?sortUsing:[`mean | `median | `max | `min | `sum | `region_length] ->
+  ?sortUsingSamples:int list ->
+  ?averageTypeSummaryPlot:[`mean | `median | `min | `max | `std | `sum] ->
+  ?missingDataColor:string ->
+  ?colorMap:string ->
+  ?alpha:float ->
+  ?colorList:string list ->
+  ?colorNumber:int ->
+  ?zMin:float list ->
+  ?zMax:float list ->
+  ?heatmapHeight:float ->
+  ?heatmapWidth:float ->
+  ?whatToShow:[`plot_heatmap_and_colorbar | `plot_and_heatmap | `heatmap_only | `heatmap_and_colorbar] ->
+  ?boxAroundHeatmaps:bool ->
+  ?xAxisLabel:string ->
+  ?startLabel:string ->
+  ?endLabel:string ->
+  ?refPointLabel:string ->
+  ?regionsLabel:string list ->
+  ?samplesLabel:string list ->
+  ?plotTitle:string ->
+  ?yAxisLabel:string ->
+  ?yMin:float list ->
+  ?yMax:float list ->
+  ?legendLocation:[`best | `upper_right | `upper_left | `upper_center | `lower_left | `lower_right | `lower_center | `center | `center_left | `center_right | `none] ->
+  ?perGroup:bool ->
+  'a img_format ->
+  deeptools_matrix gz workflow ->
+  'a workflow
+                  )
--- lib/bioinfo/deeptools.mli
+++ lib/bioinfo/deeptools.mli
@@ -1,10 +1,14 @@
 open Bistro.Std
 open Defs
 
-type 'a output
+type 'a signal_format
+val bigwig : Ucsc_gb.bigWig signal_format
+val bedgraph : Ucsc_gb.bedGraph signal_format
 
-val bigwig : Ucsc_gb.bigWig output
-val bedgraph : Ucsc_gb.bedGraph output
+type 'a img_format
+val png : png img_format
+val pdf : pdf img_format
+val svg : svg img_format
 
 val bamcoverage :
   ?scalefactor:float ->
@@ -25,7 +29,7 @@ val bamcoverage :
   ?samflagexclude:int ->
   ?minfragmentlength:int ->
   ?maxfragmentlength:int ->
-  'a output ->
+  'a signal_format ->
   [ `indexed_bam ] directory workflow ->
   'a workflow
 
@@ -54,7 +58,7 @@ val bamcompare :
   ?samflagexclude:int ->
   ?minfragmentlength:int ->
   ?maxfragmentlength:int ->
-  'a output ->
+  'a signal_format ->
   [ `indexed_bam ] directory workflow ->
   [ `indexed_bam ] directory workflow ->
   'a workflow
@@ -68,9 +72,9 @@ val bigwigcompare :
   ?region:string ->
   ?blacklistfilename: [ `bed | `gtf ] workflow ->
   ?threads:int ->
-  'a output ->
-  [ `bigwig ] workflow ->
-  [ `bigwig ] workflow ->
+  'a signal_format ->
+  Ucsc_gb.bigWig workflow ->
+  Ucsc_gb.bigWig workflow ->
   'a workflow
 
 class type compressed_numpy_array = object
@@ -117,3 +121,91 @@ val multibamsummary_bed :
   #bed3 workflow ->
   indexed_bam workflow list ->
   compressed_numpy_array workflow
+
+class type deeptools_matrix = object
+  inherit binary_file
+  method format : [`deeptools_matrix]
+end
+
+val computeMatrix_reference_point :
+  ?referencePoint:[`TSS | `TES | `center] ->
+  ?upstream:int ->
+  ?downstream:int ->
+  ?nanAfterEnd:bool ->
+  ?binSize:int ->
+  ?sortRegions:[`descend | `ascend | `no | `keep] ->
+  ?sortUsing:[`mean | `median | `max | `min | `sum | `region_length] ->
+  ?sortUsingSamples:int list ->
+  ?averageTypeBins:[`mean | `median | `min | `max | `std | `sum] ->
+  ?missingDataAsZero:bool ->
+  ?skipZeros:bool ->
+  ?minThreshold:float ->
+  ?maxThreshold:float ->
+  ?blackList:#bed3 workflow ->
+  ?scale:float ->
+  ?numberOfProcessors:int ->
+  regions:#bed3 workflow list ->
+  scores:Ucsc_gb.bigWig workflow list ->
+  unit ->
+  deeptools_matrix gz workflow
+
+val plotHeatmap :
+  ?dpi:int ->
+  ?kmeans:int ->
+  ?hclust:int ->
+  ?sortRegions:[`descend | `ascend | `no] ->
+  ?sortUsing:[`mean | `median | `max | `min | `sum | `region_length] ->
+  ?sortUsingSamples:int list ->
+  ?averageTypeSummaryPlot:[`mean | `median | `min | `max | `std | `sum] ->
+  ?missingDataColor:string ->
+  ?colorMap:string ->
+  ?alpha:float ->
+  ?colorList:string list ->
+  ?colorNumber:int ->
+  ?zMin:float list ->
+  ?zMax:float list ->
+  ?heatmapHeight:float ->
+  ?heatmapWidth:float ->
+  ?whatToShow:[`plot_heatmap_and_colorbar | `plot_and_heatmap | `heatmap_only | `heatmap_and_colorbar] ->
+  ?boxAroundHeatmaps:bool ->
+  ?xAxisLabel:string ->
+  ?startLabel:string ->
+  ?endLabel:string ->
+  ?refPointLabel:string ->
+  ?regionsLabel:string list ->
+  ?samplesLabel:string list ->
+  ?plotTitle:string ->
+  ?yAxisLabel:string ->
+  ?yMin:float list ->
+  ?yMax:float list ->
+  ?legendLocation:[`best | `upper_right | `upper_left | `upper_center | `lower_left | `lower_right | `lower_center | `center | `center_left | `center_right | `none] ->
+  ?perGroup:bool ->
+  'a img_format ->
+  deeptools_matrix gz workflow ->
+  'a workflow
+
+
+(* val plotProfile : *)
+(*   ?dpi:int -> *)
+(*   ?kmeans:int -> *)
+(*   ?hclust:int -> *)
+(*   ?averageType:[`mean | `median | `min | `max | `std | `sum] -> *)
+(*   ?plotHeight:float -> (\** in cm *\) *)
+(*   ?plotWidth:float -> *)
+(*   ?plotType:[`lines | `fill | `se | `std | `overlapped_lines | `heatmap] -> *)
+(*   ?colors:string list -> *)
+(*   ?numPlotsPerRow:int -> *)
+(*   ?startLabel:string -> *)
+(*   ?endLabel:string -> *)
+(*   ?refPointLabel:string -> *)
+(*   ?regionsLabel:string list -> *)
+(*   ?samplesLabel:string list -> *)
+(*   ?plotTitle:string -> *)
+(*   ?yAxisLabel:string -> *)
+(*   ?yMin:int list -> *)
+(*   ?yMax:int list -> *)
+(*   ?legendLocation:[`best | `upper_right | `upper_left | `upper_center | `lower_left | `lower_right | `lower_center | `center | `center_left | `center_right | `none] -> *)
+(*   ?perGroup:bool -> *)
+(*   'a output -> *)
+(*   deeptools_matrix gz workflow -> *)
+(*   'a workflow *)
