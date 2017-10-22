faf502d4172c949e035d96f84908a65e8abe41e3
Philippe Veber <philippe.veber@gmail.com>
2017-10-22 14:30:22 +0200

    bioinfor/Sra_toolkit: added more flavors to fastq_dump

--- lib/bioinfo/sra_toolkit.ml
+++ lib/bioinfo/sra_toolkit.ml
@@ -8,6 +8,16 @@ let fastq_dump sra =
     cmd ~env "fastq-dump" [ string "-Z" ; dep sra ] ~stdout:dest
   ]
 
+let sra_of_input = function
+  | `id id -> string id
+  | `file w -> dep w
+
+let fastq_dump_gz input =
+  let sra = sra_of_input input in
+  workflow ~descr:"sratoolkit.fastq_dump" [
+    cmd ~env "fastq-dump" [ string "-Z" ; sra ] ~stdout:dest
+  ]
+
 let fastq_dump_pe sra =
   let dir =
     workflow ~descr:"sratoolkit.fastq_dump" [
@@ -23,6 +33,25 @@ let fastq_dump_pe sra =
   in
   dir / selector ["reads_1.fastq"],
   dir / selector ["reads_2.fastq"]
+
+
+let fastq_dump_pe_gz input =
+  let sra = sra_of_input input in
+  let dir =
+    workflow ~descr:"sratoolkit.fastq_dump" [
+      mkdir_p dest ;
+      cmd ~env "fastq-dump" [
+        opt "-O" ident dest ;
+        string "--split-files" ;
+        string "--gzip" ;
+        sra ;
+      ] ;
+      mv (dest // "*_1.fastq*") (dest // "reads_1.fq.gz") ;
+      mv (dest // "*_2.fastq*") (dest // "reads_2.fq.gz") ;
+    ]
+  in
+  dir / selector ["reads_1.fq.gz"],
+  dir / selector ["reads_2.fq.gz"]
 
 let fastq_dump_to_fasta sra =
   workflow ~descr:"sratoolkit.fastq_dump" [
--- lib/bioinfo/sra_toolkit.mli
+++ lib/bioinfo/sra_toolkit.mli
@@ -3,6 +3,14 @@ open Defs
 
 val fastq_dump : sra workflow -> [`sanger] fastq workflow
 
+val fastq_dump_gz :
+  [`id of string | `file of sra workflow] ->
+  [`sanger] fastq gz workflow
+
 val fastq_dump_pe : sra workflow -> [`sanger] fastq workflow * [`sanger] fastq workflow
+
+val fastq_dump_pe_gz :
+  [`id of string | `file of sra workflow] ->
+  [`sanger] fastq gz workflow * [`sanger] fastq gz workflow
 
 val fastq_dump_to_fasta : sra workflow -> fasta workflow
