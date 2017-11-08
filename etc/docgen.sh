09809a9a2f3411c8590543d5aa4bd95e220d95bc
Philippe Veber <philippe.veber@gmail.com>
2017-11-08 11:58:07 +0100

    etc: added docgen + publish script

---
+++ etc/docgen.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+jbuilder build @doc
+
+git clone git@github.com:pveber/bistro.git
+cd bistro
+git checkout gh-pages
+rsync -avz ../_build/default/_doc/ ./
+git add *
+git commit -m $(cd .. && git log -1 --pretty="%h %s")
+git push origin gh-pages
+cd ..
+rm -rf bistro
