237c549c47eb5f9593a5547d875ce66188ccc6bc
Philippe Veber <philippe.veber@gmail.com>
2017-11-08 12:22:19 +0100

    bistro: small fix on doc

--- lib/bistro.mli
+++ lib/bistro.mli
@@ -2,9 +2,10 @@
 
     This module introduces a type ['a worfklow] that describe a set of
     inter-dependent actions that will eventually generate a target
-    (file or directory). The ['a] type represents the format of the
-    file or the layout of the directory. Actions may be either command
-    lines to be executed, or OCaml expressions to be evaluated.
+    (file or directory). The ['a] type variable represents the format
+    of the file or the layout of the directory. Actions may be either
+    command lines to be executed, or OCaml expressions to be
+    evaluated.
 
     To build workflows, use the {!module:EDSL} module, that provide a set of
     combinators to write shell scripts easily. For instance, the
@@ -21,7 +22,7 @@
     result. Building the workflow won't actually generate anything. In
     order to run the workflow, you have to run it using an execution
     engine like the one provided by [bistro.engine].
- *)
+*)
 
 
 (** Helper functions to represent paths as string lists. For absolute
