pveber/bistro

commit 3fb7647780ec452be5f3dfba9a5eedc4c4c8f98a

engine/Task: safer implementation of failure test

lib/engine/task.ml
@@ -458,7 +458,8 @@ let post_revdeps_hook t config ~all_revdeps_succeeded =
 let failure = function
   | Input_check { pass ; _ }
   | Select_check { pass ; _ } -> not pass
-  | Step_result { outcome ; _ } -> outcome <> `Succeeded
+  | Step_result { outcome = `Succeeded ; _ } -> true
+  | Step_result { outcome = (`Failed | `Missing_output) ; _ } -> false
 
 let render_step_command ~np ~mem config task cmd =
   let open Concrete_task in

commit fc17a8d85b7d7e2bb43caecbe703447caaa5082f

fixed bug introduced in 3fb7647

lib/engine/task.ml
@@ -458,8 +458,8 @@ let post_revdeps_hook t config ~all_revdeps_succeeded =
 let failure = function
   | Input_check { pass ; _ }
   | Select_check { pass ; _ } -> not pass
-  | Step_result { outcome = `Succeeded ; _ } -> true
-  | Step_result { outcome = (`Failed | `Missing_output) ; _ } -> false
+  | Step_result { outcome = `Succeeded ; _ } -> false
+  | Step_result { outcome = (`Failed | `Missing_output) ; _ } -> true
 
 let render_step_command ~np ~mem config task cmd =
   let open Concrete_task in