ec1b08fb388cc25076eb25e6df6233a336eff37c
Philippe Veber <philippe.veber@gmail.com>
2018-02-21 18:16:09 +0100

    test/Map_command: run more commands in parallel

--- lib/test/map_command.ml
+++ lib/test/map_command.ml
@@ -3,19 +3,26 @@ open Bistro.Std
 open Bistro.EDSL
 open Bistro_utils
 
-let dir = workflow [
-    mkdir_p dest ;
-    cmd "echo" ~stdout:(dest // "a") [ string "a" ] ;
-    cmd "echo" ~stdout:(dest // "b") [ string "b" ] ;
-  ]
+let dir = workflow (
+    mkdir_p dest
+    :: List.init 20 ~f:(fun i ->
+        let id = sprintf "f%d" i in
+        cmd "echo" ~stdout:(dest // id) [ string id ]
+      )
+  )
 
 let pwc =
-  map_command dir (fun x -> cmd "wc" ~stdout:dest [ dep x ])
+  map_command ~np:4 dir (fun x ->
+      and_list [
+        cmd "sleep" [ int 1 ] ;
+        cmd "wc" ~stdout:dest [ dep x ] ;
+      ]
+    )
 
 let main () =
   let logger = Console_logger.create () in
   let repo = Repo.[ ["output"] %> pwc ] in
-  Repo.build ~logger repo ~outdir:"out"
+  Repo.build ~np:4 ~logger repo ~outdir:"out"
   |> ignore
 
 let command =
