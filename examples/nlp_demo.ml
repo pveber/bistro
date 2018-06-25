30eb5a08eda2d857b89d9e9eb79b9479ec323a14
Philippe Veber <philippe.veber@gmail.com>
2018-06-25 20:52:59 +0200

    started a small nlp demo

---
+++ examples/nlp_demo.ml
@@ -0,0 +1,18 @@
+open Bistro_pack
+
+let wikipedia_query q : text_file workflow =
+  let url = "https://en.wikipedia.org/api/rest_v1/page/summary/" ^ q in
+  shell ~descr:"wikipedia_query" Shell_dsl.[
+    pipe [
+      cmd "curl" [
+        quote ~using:'\'' (string url) ;
+      ] ;
+      cmd "sed" ~stdout:dest [ string {|-n 's/.*"extract":"\([^"]*\)".*/\1/p'|} ] ;
+    ]
+  ]
+
+let () =
+  let open Repo in
+  build ~outdir:"res" [
+    item [ "text" ; "protein.txt" ] (wikipedia_query "Protein") ;
+  ]
