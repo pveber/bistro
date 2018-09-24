49f8fd217cd80f2e4986d5d6bfdf4e3b88e276a4
Philippe Veber <philippe.veber@gmail.com>
2018-09-24 10:04:12 +0200

    examples/Workflow_list: more logging

--- examples/workflow_example.ml
+++ examples/workflow_example.ml
@@ -1,16 +1,18 @@
-open Bistro
+open Bistro_pack
 open Shell_dsl
 
 let echo msg file = cmd "echo" ~stdout:(dest // file) [ string msg ]
 
-let dir : [`ABC] directory workflow = shell [
+let dir : [`ABC] directory workflow =
+  shell ~descr:"abc-dir" [
     mkdir_p dest ;
     echo "foo" "a" ;
     echo "bar" "b" ;
     echo "bazinga" "c" ;
   ]
 
-let wc (f : text_file workflow) : text_file workflow = shell [
+let wc (f : text_file workflow) : text_file workflow =
+  shell ~descr:"wc" [
     cmd "wc" ~stdout:dest [ dep f ] ;
   ]
 
@@ -22,4 +24,7 @@ let repo = Bistro_engine.Repo.[
     items ["wc"] wc_dir ;
   ]
 
-let () = Bistro_engine.Repo.build ~outdir:"delme" repo
+let () =
+  Bistro_engine.Repo.build
+    ~loggers:[console_logger ()]
+    ~outdir:"delme" repo
