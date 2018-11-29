f45c38ee1b3de453ef2154903188d2fe50340b11
Philippe Veber <philippe.veber@gmail.com>
2018-11-29 11:22:56 +0100

    imported shell and template dsl

--- lib/bistro.ml
+++ lib/bistro.ml
@@ -1,3 +1,4 @@
+open Base
 open Bistro_internals
 
 type 'a path = Workflow.path
@@ -12,4 +13,122 @@ module Workflow = Workflow
 
 module Private = struct
   let reveal x = x
+end
+
+
+class type file = object
+  method file_type : [`regular]
+end
+
+class type text_file = object
+  inherit file
+  method encoding : [`text]
+end
+
+class type binary_file = object
+  inherit file
+  method encoding : [`binary]
+end
+
+module Template_dsl = struct
+  type template = Workflow.path Workflow.t Template.t
+
+  let dest = [ Template.DEST ]
+  let tmp = [ Template.TMP ]
+  let np = [ Template.NP ]
+  let mem = [ Template.MEM ]
+
+  let string s = [ Template.S s ]
+  let int i = string (Int.to_string i)
+  let float f = string (Float.to_string f)
+  let dep w = [ Template.D w ]
+
+  let quote ?using:(c = '"') e =
+    let quote_symbol = Template.S (Char.to_string c) in
+    quote_symbol :: e @ [ quote_symbol ]
+
+  let option f = function
+    | None -> []
+    | Some x -> f x
+
+  let list f ?(sep = ",") l =
+    List.map l ~f
+    |> List.intersperse ~sep:(string sep)
+    |> List.concat
+
+  let seq ?sep xs =
+    let format = match sep with
+      | None -> Fn.id
+      | Some sep -> List.intersperse ~sep:(string sep)
+    in
+    List.concat (format xs)
+
+  let enum dic x = string (List.Assoc.find_exn ~equal:Caml.( = ) dic x)
+
+  let file_dump contents = [ Template.F contents ] (* FIXME: should check that there is no file_dump in contents *)
+end
+
+module Shell_dsl = struct
+  include Template_dsl
+
+  type command = Workflow.shell_command
+  type docker_image = Command.docker_image
+
+  let docker image cmd = Command.Docker (image, cmd)
+
+  let gen_cmd prog_expr ?env ?stdin ?stdout ?stderr args =
+    let stdout_expr =
+      match stdout with
+      | None -> []
+      | Some e -> Template.S " > " :: e
+    in
+    let stdin_expr =
+      match stdin with
+      | None -> []
+      | Some e -> Template.S " < " :: e
+    in
+    let stderr_expr =
+      match stderr with
+      | None -> []
+      | Some e -> Template.S " 2> " :: e
+    in
+    let tokens =
+      [ prog_expr ] @ args @ [ stdin_expr ; stdout_expr ; stderr_expr ]
+      |> List.filter ~f:(Caml.( <> ) [])
+      |> List.intersperse ~sep:(string " ")
+      |> List.concat
+    in
+    let cmd = Command.Simple_command tokens in
+    match env with
+    | None -> cmd
+    | Some image -> docker image cmd
+
+  let cmd p = gen_cmd [ S p ]
+
+  let opt o f x = Template.(S o :: S " " :: f x)
+
+  let opt' o f x = Template.(S o :: S "=" :: f x)
+
+  let flag f x b = if b then f x else []
+
+  let mkdir d = cmd "mkdir" [ d ]
+
+  let mkdir_p d = cmd "mkdir" [ string "-p" ; d ]
+
+  let cd p = cmd "cd" [ p ]
+
+  let rm_rf x = cmd "rm" [ string "-rf" ; x ]
+
+  let mv x y = cmd "mv" [ x ; y ]
+
+  let ( // ) x y = Template.(x @ [ S "/" ; S y ])
+
+  let or_list xs = Command.Or_list xs
+  let and_list xs = Command.And_list xs
+  let pipe xs = Command.Pipe_list xs
+
+
+  let ( % ) f g x = g (f x)
+
+  let docker_image = Command.docker_image
 end
--- lib/bistro.mli
+++ lib/bistro.mli
@@ -6,6 +6,128 @@ class type directory = object
   method file_kind : [`directory]
 end
 
+module Template_dsl : sig
+  type template
+
+  val dest : template
+  (** Symbol representing the location where a workflow is expected to
+      produce its result *)
+
+  val tmp : template
+  (** Symbol representing an existing empty directory that can be used
+      as a temporary space for a workflow's execution. *)
+
+  val np : template
+  (** Symbol representing the number of cores allocated to the
+      workflow *)
+
+  val mem : template
+  (** Symbol representing the memory size allocated to the workflow,
+      in GB. *)
+
+  val string : string -> template
+  (** A chunk of text *)
+
+  val int : int -> template
+  (** Int formatting *)
+
+  val float : float -> template
+  (** Float formatting *)
+
+  val dep : _ path workflow -> template
+  (** [dep w] is interpreted as the path where to find the result of
+      workflow [w] *)
+
+  val quote : ?using:char -> template -> template
+  (** [quote ~using:c t] surrounds template [t] with character [c] *)
+
+  val option : ('a -> template) -> 'a option -> template
+  (** [option f o] is [f x] if [o = Some x] and [string ""]
+      otherwise *)
+
+  val list : ('a -> template) -> ?sep:string -> 'a list -> template
+  (** list combinator, optional value of [sep] is [","] *)
+
+  val seq : ?sep:string -> template list -> template
+  (** another list combinator, default value for [sep] is [""] *)
+
+  val enum : ('a * string) list -> 'a -> template
+  (** combinator for enumerations *)
+
+  val file_dump : template -> template
+  (** [file_dump t] can be used when a command needs a configuration
+      script: at run-time, it will generate a text using [t], save it
+      to a path, deterministically chosen as a function of
+      [t]. Finally the template [file_dump t] is interpreted as this
+      path. *)
+end
+
+module Shell_dsl : sig
+  type template
+  type command
+  type docker_image
+
+  include module type of Template_dsl with type template := template
+
+  val cmd :
+    string ->
+    ?env:docker_image ->
+    ?stdin:template -> ?stdout:template -> ?stderr:template ->
+    template list -> command
+  (** Command-line constructor, e.g. [cmd "echo" ~stdout:dest [ string
+      "foo" ]] will generate a shell command like ["echo foo >
+      /some/path"].
+      - @param env specifies a Docker image where to run the command
+      - @param stdin adds a ["< /some/path"] token at the end of the command
+      - @param stdout adds a ["> /some/path"] token at the end of the command
+      - @param stderr adds a ["2> /some/path"] token at the end of the command *)
+
+  val opt : string -> ('a -> template) -> 'a -> template
+  (** Command-line option formatting, e.g.: [opt "--output" dep dest]
+      will be rendered like ["--output /some/path"] *)
+
+  val opt' : string -> ('a -> template) -> 'a -> template
+  (** Same as {!val:opt} but renders options with an equal sign,
+      e.g. ["--output=/some/path"] *)
+
+  val flag : ('a -> template) -> 'a -> bool -> template
+  (** [flag f x b] renders as [f x] if [b] is true *)
+
+  val or_list : command list -> command
+  (** OR-sequence of commands ([ || ]) *)
+
+  val and_list : command list -> command
+  (** AND-sequence of commands ([ && ]) *)
+
+  val pipe : command list -> command
+  (** Pipe of commands ([ | ]) *)
+
+  val ( // ) : template -> string -> template
+  (** Similar to {!val:Filename.concat}, but with other types. *)
+
+  (** {5 Useful commands} *)
+
+  val mkdir : template -> command
+  val mkdir_p : template -> command
+  val cd : template -> command
+  val rm_rf : template -> command
+  val mv : template -> template -> command
+
+  val docker : docker_image -> command -> command
+  (** [docker cmd] transforms [cmd] so that it can be executed in a
+      Docker container. *)
+
+  val docker_image :
+    ?tag:string ->
+    ?registry:string ->
+    account:string ->
+    name:string ->
+    unit -> docker_image
+  (** Construct a description of a publicly available docker image *)
+
+  val ( % ) : ('a -> 'b) -> ('b -> 'c) -> 'a -> 'c
+end
+
 module Workflow : sig
   val cached_value :
     ?descr:string ->
@@ -32,6 +154,17 @@ module Workflow : sig
     string list ->
     'a path workflow
 
+  val shell :
+    ?descr:string ->
+    ?mem:int ->
+    ?np:int ->
+    ?version:int ->
+    Shell_dsl.command list -> 'a path workflow
+  (** Workflow constructor, taking a list of commands in input. Other arguments are:
+      - @param descr description of the workflow, used for logging
+      - @param mem required memory
+      - @param np maximum number of cores (could be given less at execution)
+      - @param version version number, used to force the rebuild of a workflow *)
 
   val pure : id:string -> 'a -> 'a workflow
   val pure_data : 'a -> 'a workflow
@@ -48,6 +181,23 @@ module Workflow : sig
     'b list workflow
 end
 
+
 module Private : sig
   val reveal : 'a workflow -> 'a Bistro_internals.Workflow.t
+end
+
+(** {5 File formats} *)
+
+class type file = object
+  method file_type : [`regular]
+end
+
+class type text_file = object
+  inherit file
+  method encoding : [`text]
+end
+
+class type binary_file = object
+  inherit file
+  method encoding : [`binary]
 end
