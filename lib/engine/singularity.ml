a0cc0eee098e6149ded505194ab35950c349cd94
Philippe Veber <philippe.veber@gmail.com>
2019-01-02 11:31:36 +0100

    engine/Singularity: fixed fetch

--- lib/engine/singularity.ml
+++ lib/engine/singularity.ml
@@ -2,15 +2,19 @@ open Core_kernel
 open Bistro_internals
 open Lwt.Infix
 
+let tag = function
+  | None -> ""
+  | Some t -> ":" ^ t
+
 let url_of_image : Command.container_image -> string = function
   | Docker_image img ->
     sprintf "docker://%s/%s%s"
       img.account img.name
-      (Option.value ~default:"" img.tag)
+      (tag img.tag)
   | Singularity_image img ->
     sprintf "shub://%s/%s%s"
       img.account img.name
-      (Option.value ~default:"" img.tag)
+      (tag img.tag)
 
 let fetch_image db img =
   let url = url_of_image img in
@@ -20,7 +24,7 @@ let fetch_image db img =
     url
   |]
   in
-  Lwt_process.exec ("", cmd) >>= function
+  Lwt_process.exec ~stderr:`Dev_null ~stdout:`Dev_null ("", cmd) >>= function
   | WEXITED 0 -> Lwt_result.return ()
   | WEXITED n
   | WSIGNALED n
