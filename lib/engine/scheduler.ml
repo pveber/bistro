pveber/bistro

commit c8556cd3d7b7aff89a469252e3cc655c9acfcf11

Scheduler: pworkflow missing dest

lib/engine/scheduler.ml
@@ -537,6 +537,30 @@ let perform_shell sched (Allocator.Resource { np ; mem }) ~id ~descr cmd =
       stderr = env.stderr ;
     })
 
+let perform_path_plugin sched (Allocator.Resource { mem ; np }) f ~id ~descr =
+  let env =
+    Execution_env.make
+      ~allowed_containers:[]
+      ~db:sched.db
+      ~np ~mem ~id
+  in
+  let cache_dest = Db.cache sched.db id in
+  worker (Fn.flip f env.dest) () >>= function
+  | Ok () ->
+    let outcome =
+      if Sys.file_exists env.dest = `Yes then `Succeeded
+      else `Missing_output
+    in
+    Misc.(
+      if outcome = `Succeeded then
+        mv env.dest cache_dest
+      else Lwt.return ()
+    ) >>= fun () ->
+    Lwt_result.return (Task_result.Plugin { id ; outcome ; msg = None ; descr })
+  | Error msg ->
+    Lwt_result.return (Task_result.Plugin { id ; outcome = `Failed ; msg = Some msg ; descr })
+
+
 let rec blocking_evaluator
   : type s. Db.t -> s W.t -> (unit -> s)
   = fun db w ->
@@ -830,19 +854,9 @@ let rec build
     | W.Plugin { id ; task = Path_plugin workflow ; descr ; _ } ->
       schedule_cached_workflow sched ~id w
         ~deps:(fun () -> build sched ~target:w workflow)
-        ~perform:(fun _ ->
-          let evaluator = blocking_evaluator sched.db workflow in
-          (* let env = *) (* FIXME: use this *)
-          (*   Execution_env.make *)
-          (*     ~use_docker:sched.use_docker *)
-          (*     ~db:sched.db *)
-          (*     ~np ~mem ~id *)
-          (* in *)
-          worker (Fn.flip evaluator (Db.cache sched.db id)) () >|=
-          function
-          | Ok () ->
-            Ok (Task_result.Plugin { id ; outcome = `Succeeded ; msg = None ; descr })
-          | Error msg -> Ok (Task_result.Plugin { id ; outcome = `Failed ; msg = Some msg ; descr })
+        ~perform:(fun resource ->
+            let evaluator = blocking_evaluator sched.db workflow in
+            perform_path_plugin sched resource ~id ~descr evaluator
         )
 
     | W.Shell { id ; task ; descr ; deps ; _ } ->

commit 982836b104637997e6a89bf1469c949d5206a26c

engine/Scheduler: fixed bug introduced in c8556cd

lib/engine/scheduler.ml
@@ -545,6 +545,8 @@ let perform_path_plugin sched (Allocator.Resource { mem ; np }) f ~id ~descr =
       ~np ~mem ~id
   in
   let cache_dest = Db.cache sched.db id in
+  Misc.remove_if_exists env.tmp_dir >>= fun () ->
+  Unix.mkdir_p env.tmp ;
   worker (Fn.flip f env.dest) () >>= function
   | Ok () ->
     let outcome =
@@ -553,7 +555,8 @@ let perform_path_plugin sched (Allocator.Resource { mem ; np }) f ~id ~descr =
     in
     Misc.(
       if outcome = `Succeeded then
-        mv env.dest cache_dest
+        mv env.dest cache_dest >>= fun () ->
+        remove_if_exists env.tmp_dir
       else Lwt.return ()
     ) >>= fun () ->
     Lwt_result.return (Task_result.Plugin { id ; outcome ; msg = None ; descr })