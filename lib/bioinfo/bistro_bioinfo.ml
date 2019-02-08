5233c2bb06ab7614a4738715f763d8e8d3b669fc
Philippe Veber <philippe.veber@gmail.com>
2019-02-08 16:41:37 +0100

    bioinfo: added Hisat2

--- lib/bioinfo/bistro_bioinfo.ml
+++ lib/bioinfo/bistro_bioinfo.ml
@@ -2582,3 +2582,80 @@ module Quast = struct
       ]
     ]
 end
+
+module Hisat2 = struct
+  let img = [ docker_image ~account:"pveber" ~name:"hisat2" ~tag:"2.1.0" () ]
+
+  let hisat2_build ?large_index ?noauto ?packed ?bmax ?bmaxdivn ?dcv ?nodc ?noref ?justref ?offrate ?ftabchars ?seed ?cutoff fa =
+    Workflow.shell ~descr:"hisat2-build" ~mem:(Workflow.int (8 * 1024)) ~np:8 [
+      mkdir_p dest ;
+      cmd "hisat2-build" ~img [
+        option (flag string "--large-index") large_index ;
+        option (flag string "--no-auto") noauto ;
+        option (flag string "--packed") packed ;
+        option (flag string "--nodc") nodc ;
+        option (flag string "--noref") noref ;
+        option (flag string "--justref") justref ;
+        option (opt "--bmax" int) bmax ;
+        option (opt "--bmaxdivn" int) bmaxdivn ;
+        option (opt "--dcv" int) dcv ;
+        option (opt "--offrate" int) offrate ;
+        option (opt "--ftabchars" int) ftabchars ;
+        opt "--threads" Fn.id np ;
+        option (opt "--seed" int) seed ;
+        option (opt "--cutoff" int) cutoff ;
+        opt "-f" dep fa ;
+        seq [ dest ; string "/index" ]
+      ]
+    ]
+
+  let qual_option (type s) x = match (x : s Fastq.format) with
+    | Fastq.Solexa  -> "--solexa-quals"
+    | Fastq.Sanger -> "--phred33-quals"
+    | Fastq. Phred64 -> "--phred64-quals"
+
+  let flag_of_orientation = function
+    | `fr -> "--fr"
+    | `rf -> "--rf"
+    | `ff -> "--ff"
+
+  let hisat2
+      ?skip ?qupto ?trim5 ?trim3 ?fastq_format
+      ?k
+      ?minins ?maxins ?orientation ?no_mixed ?no_discordant
+      ?seed
+      index
+      fqs
+    =
+    let args = match fqs with
+      | `single_end fqs ->
+        opt "-U" (list dep ~sep:",") fqs
+      | `paired_end (fqs1, fqs2) ->
+        seq [
+          opt "-1" (list dep ~sep:",") fqs1 ;
+          string " " ;
+          opt "-2" (list dep ~sep:",") fqs2
+        ]
+    in
+    Workflow.shell ~descr:"hisat2" ~mem:(Workflow.int (4 * 1024)) ~np:8 [
+      cmd "hisat2" ~img [
+        option (opt "--skip" int) skip ;
+        option (opt "--qupto" int) qupto ;
+        option (opt "--trim5" int) trim5 ;
+        option (opt "--trim3" int) trim3 ;
+        option (opt "-k" int) k ;
+        option (opt "--minins" int) minins ;
+        option (opt "--maxins" int) maxins ;
+        option (flag_of_orientation % string) orientation ;
+        option (flag string "--no-mixed") no_mixed  ;
+        option (flag string "--no-discordant") no_discordant  ;
+        opt "--threads" Fn.id np ;
+        option (opt "--seed" int) seed ;
+        option (opt "-q" (qual_option % string)) fastq_format ;
+
+        opt "-x" (fun index -> seq [dep index ; string "/index"]) index ;
+        args ;
+        opt "-S" Fn.id dest ;
+      ]
+    ]
+end
--- lib/bioinfo/bistro_bioinfo.mli
+++ lib/bioinfo/bistro_bioinfo.mli
@@ -828,6 +828,46 @@ module Tophat : sig
   val junctions : output pworkflow -> bed6 pworkflow
 end
 
+module Hisat2 : sig
+  val img : Shell_dsl.container_image list
+
+  val hisat2_build :
+    ?large_index:bool ->
+    ?noauto:bool ->
+    ?packed:bool ->
+    ?bmax:int ->
+    ?bmaxdivn:int ->
+    ?dcv:int ->
+    ?nodc:bool ->
+    ?noref:bool ->
+    ?justref:bool ->
+    ?offrate:int ->
+    ?ftabchars:int ->
+    ?seed:int ->
+    ?cutoff:int ->
+    fasta pworkflow ->
+    [`hisat2_index] dworkflow
+
+
+  val hisat2 :
+    ?skip:int ->
+    ?qupto:int ->
+    ?trim5:int ->
+    ?trim3:int ->
+    ?fastq_format:'a Fastq.format ->
+    ?k:int ->
+    ?minins:int ->
+    ?maxins:int ->
+    ?orientation:[`fr | `ff | `rf] ->
+    ?no_mixed:bool ->
+    ?no_discordant:bool ->
+    ?seed:int ->
+    [`hisat2_index] dworkflow ->
+    [ `single_end of sanger_fastq pworkflow list
+    | `paired_end of sanger_fastq pworkflow list * sanger_fastq pworkflow list ] ->
+    sam pworkflow
+end
+
 module Kallisto : sig
   class type index = object
     inherit binary_file
