0f51f4bca8399615d5106531cd63c68dcf0358d0
Philippe Veber <philippe.veber@gmail.com>
2019-07-18 17:40:23 +0200

    bioinfo: added Idr

--- lib/bioinfo/bistro_bioinfo.mli
+++ lib/bioinfo/bistro_bioinfo.mli
@@ -1215,6 +1215,31 @@ module Macs2 : sig
   val broad_peaks : broad_output pworkflow -> broad_peaks pworkflow
 end
 
+module Idr : sig
+  type 'a format
+
+  val narrowPeak : Macs2.narrow_peaks format
+  val broadPeak : Macs2.broad_peaks format
+  val bed : bed3 format
+  val gff : gff format
+
+  type 'a output = [`idr_output of 'a]
+
+  val idr :
+    input_file_type:'a format ->
+    ?idr_threshold:float ->
+    ?soft_idr_threshold:float ->
+    ?peak_merge_method:[ `sum | `avg | `min | `max] ->
+    ?rank:[ `signal | `pvalue | `qvalue ] ->
+    ?random_seed:int ->
+    ?peak_list:'a pworkflow ->
+    'a pworkflow ->
+    'a pworkflow ->
+    'a output dworkflow
+
+  val items : 'a output dworkflow -> 'a pworkflow
+  val figure : _ output dworkflow -> png pworkflow
+end
 
 module Meme_suite : sig
   class type meme_output = object
