12f4ded401b2c1b710eb40d225d7a2db0fe31ef1
Philippe Veber <philippe.veber@gmail.com>
2020-03-01 16:28:51 +0100

    revert 6835b3b

--- lib/engine/singularity.ml
+++ lib/engine/singularity.ml
@@ -18,7 +18,7 @@ let url_of_image : Command.container_image -> string = function
 
 let fetch_image img dest =
   let url = url_of_image img in
-  let cmd = [| "singularity" ; "pull" ; "--name" ; dest ; url |] in
+  let cmd = [| "singularity" ; "pull" ; dest ; url |] in
   Lwt_process.exec ~stderr:`Dev_null ~stdout:`Dev_null ("", cmd) >>= function
   | WEXITED 0 -> Lwt_result.return ()
   | WEXITED n
