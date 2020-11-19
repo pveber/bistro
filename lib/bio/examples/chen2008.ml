fe7c38efeeadfec4a4a8331aa5e4457a09065688
Philippe Veber <philippe.veber@gmail.com>
2020-11-20 00:35:11 +0100

    import examples from biotope

---
+++ lib/bio/examples/chen2008.ml
@@ -0,0 +1,53 @@
+open Core_kernel
+open Bistro
+open Bistro_bio
+open Biotk
+
+module Sample = struct
+  type t = [
+    | `ES_WT_ChIP_Nanog_Chen2008
+    | `ES_WT_ChIP_Pou5f1_Chen2008
+    | `ES_WT_ChIP_Sox2_Chen2008
+    | `ES_WT_ChIP_Essrb_Chen2008
+  ]
+  [@@deriving show,enumerate]
+
+  let srr_id = function
+    | `ES_WT_ChIP_Nanog_Chen2008 -> List1.cons "SRR002004" ["SRR002005";"SRR002006";"SRR002007";"SRR002008";"SRR002009";"SRR002010";"SRR002011"]
+    | `ES_WT_ChIP_Pou5f1_Chen2008 -> List1.cons "SRR002012" ["SRR002013";"SRR002014";"SRR002015"]
+    | `ES_WT_ChIP_Sox2_Chen2008 -> List1.cons "SRR002023" ["SRR002024";"SRR002025";"SRR002026"]
+    | `ES_WT_ChIP_Essrb_Chen2008 -> List1.cons "SRR001992" ["SRR001993";"SRR001994";"SRR001995"]
+
+  let source x =
+    List1.map (srr_id x) ~f:(fun srr_id -> Fastq_sample.SRA_dataset { srr_id ; library_type = `single_end })
+
+  let string_of_sample = function
+    | `ES_WT_ChIP_Nanog_Chen2008 -> "ES_WT_ChIP_Nanog_Chen2008"
+    | `ES_WT_ChIP_Pou5f1_Chen2008 -> "ES_WT_ChIP_Pou5f1_Chen2008"
+    | `ES_WT_ChIP_Sox2_Chen2008 -> "ES_WT_ChIP_Sox2_Chen2008"
+    | `ES_WT_ChIP_Essrb_Chen2008 -> "ES_WT_ChIP_Essrb_Chen2008"
+
+  let base_url = "ftp://ftp.ncbi.nlm.nih.gov/pub/geo/DATA/supplementary/samples/GSM288nnn/"
+
+  let published_peaks_url_suffix = function
+    | `ES_WT_ChIP_Nanog_Chen2008 -> "GSM288345/GSM288345_ES_Nanog.txt.gz"
+    | `ES_WT_ChIP_Pou5f1_Chen2008 -> "GSM288346/GSM288346_ES_Oct4.txt.gz"
+    | `ES_WT_ChIP_Sox2_Chen2008 -> "GSM288347/GSM288347_ES_Sox2.txt.gz"
+    | `ES_WT_ChIP_Essrb_Chen2008 -> "GSM288355/GSM288355%5FES%5FEsrrb%2Etxt%2Egz"
+
+  let published_peaks x : text file =
+    let url = base_url ^ published_peaks_url_suffix x in
+    Bistro_unix.(wget url |> gunzip |> crlf2lf)
+
+  let to_string x = show x
+
+  let reference_genome _ = Dnaseq_with_reference_genome.Ucsc_gb `mm10
+end
+
+module FQS = Fastq_sample.Make(Sample)
+
+module Dnaseq = Dnaseq_with_reference_genome.Make(struct
+    include Sample
+    include FQS
+  end
+  )
---
+++ lib/bio/examples/zhou2011.ml
@@ -0,0 +1,231 @@
+(**
+   Paper: https://www.ncbi.nlm.nih.gov/pubmed/21700227
+   Datasets: https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE29506
+*)
+open Core
+open Biotk
+open Bistro
+open Bistro_bio
+open Bistro_bio.Formats
+open Bistro_utils
+
+let np = 4
+
+type chIP_sample = [
+  | `ChIP_Pho4_noPi
+  | `ChIP_Pho4_highPi
+  | `ChIP_Cbf1_noPi
+  | `ChIP_Mock_noPi
+]
+[@@deriving show, enumerate]
+
+type input_sample = [
+  | `Input_Pho4_noPi
+  | `Input_Pho4_highPi
+  | `Input_Cbf1_noPi
+  | `Input_Mock_noPi
+]
+[@@deriving show, enumerate]
+
+type sample = [
+  | chIP_sample
+  | input_sample
+]
+[@@deriving show, enumerate]
+
+type factor = [
+  | `Pho4
+  | `Cbf1
+  | `Mock
+]
+[@@deriving show, enumerate]
+
+type condition = [
+  | `noPi
+  | `highPi
+]
+[@@deriving show, enumerate]
+
+let factor = function
+  | `ChIP_Pho4_highPi
+  | `ChIP_Pho4_noPi -> `Pho4
+  | `ChIP_Cbf1_highPi
+  | `ChIP_Cbf1_noPi -> `Cbf1
+  | `ChIP_Mock_highPi
+  | `ChIP_Mock_noPi -> `Mock
+
+let condition = function
+  | `ChIP_Mock_highPi
+  | `ChIP_Pho4_highPi
+  | `ChIP_Cbf1_highPi -> `highPi
+  | `ChIP_Pho4_noPi
+  | `ChIP_Cbf1_noPi
+  | `ChIP_Mock_noPi -> `noPi
+
+let control_sample = function
+  | `ChIP_Cbf1_noPi -> `Input_Cbf1_noPi
+  | `ChIP_Pho4_noPi -> `Input_Pho4_noPi
+  | `ChIP_Pho4_highPi -> `Input_Pho4_highPi
+  | `ChIP_Mock_noPi -> `Input_Mock_noPi
+
+let genome = Ucsc_gb.genome_sequence `sacCer2
+let genome_2bit = Ucsc_gb.genome_2bit_sequence `sacCer2
+
+let srr_id = function
+  | `ChIP_Pho4_noPi -> [ "SRR217304" ; "SRR217305" ]
+  | `ChIP_Pho4_highPi -> [ "SRR217306" ]
+  | `ChIP_Cbf1_noPi -> [ "SRR217310" ]
+  | `ChIP_Mock_noPi -> [ "SRR217312" ]
+  | `Input_WT_noPi -> [ "SRR217324" ]
+  | `Input_Pho4_noPi -> [ "SRR217319" ]
+  | `Input_Pho4_highPi -> [ "SRR217320" ]
+  | `Input_Cbf1_noPi -> [ "SRR217323" ]
+  | `Input_Mock_noPi -> [ "SRR217324" ]
+
+let fastq x =
+  srr_id x
+  |> List1.of_list_exn
+  |> List1.map ~f:(fun id ->
+    Sra_toolkit.(fastq_dump fastq_gz) (`id id)
+    |> Fastq_sample.compressed_se
+  )
+
+let ecoli_genome : fasta file =
+  Bistro_unix.wget "ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz"
+  |> Bistro_unix.gunzip
+
+let fastq_screen x =
+  Fastq_screen.fastq_screen
+    (List1.hd (fastq x))
+    ["E_coli", ecoli_genome]
+
+let bowtie_index = Bowtie.bowtie_build genome
+
+let mapped_reads x =
+  let Cons (fq, additional_samples) = fastq x in
+  Bowtie.bowtie ~v:1 bowtie_index ~additional_samples fq
+
+let mapped_reads_bam x =
+  Samtools.indexed_bam_of_sam (mapped_reads x)
+
+let tf_peaks ?qvalue treatment_sample =
+  let control_sample = control_sample treatment_sample in
+  let treatment = mapped_reads treatment_sample in
+  let control = mapped_reads control_sample in
+  Macs2.callpeak ~mfold:(1,100) ?qvalue Macs2.sam ~control:[ control ] [ treatment ]
+
+let centered_tf_peaks ?qvalue ~radius treatment_sample =
+  let summits = Macs2.peak_summits (tf_peaks ?qvalue treatment_sample) in
+  let chrom_sizes = Ucsc_gb.fetchChromSizes `sacCer2 in
+  Bedtools.(slop ~mode:(`both radius) bed summits chrom_sizes)
+
+let best_macs_summits ?qvalue ~n sample =
+  let summits = Macs2.peak_summits (tf_peaks ?qvalue sample) in
+  let open Bistro.Shell_dsl in
+  Bistro.Workflow.shell ~descr:"best_macs_summits" [
+    pipe [
+      cmd "sort" [
+        string "-r -g -k 5" ;
+        dep summits ;
+      ] ;
+      cmd "head" ~stdout:dest [
+        opt "-n" int n ;
+      ]
+    ]
+  ]
+
+let best_peak_sequences ?(nseqs = Int.max_value) ?qvalue ~radius treatment_sample =
+  let summits = best_macs_summits ?qvalue ~n:nseqs treatment_sample in
+  let chrom_sizes = Ucsc_gb.fetchChromSizes `sacCer2 in
+  let regions = Bedtools.(slop ~mode:(`both radius) bed summits chrom_sizes) in
+  Ucsc_gb.twoBitToFa genome_2bit (Bed.keep4 regions)
+
+let meme ?(nseqs = 500) treatment_sample =
+  best_peak_sequences ~nseqs ~qvalue:1e-10 ~radius:50 treatment_sample
+  |> Meme_suite.meme ~nmotifs:3 ~minw:5 ~maxw:8 ~revcomp:true ~alphabet:`dna ~maxsize:1_000_000
+
+let meme_motifs treatment_sample =
+  Bistro.Workflow.select (meme treatment_sample) ["meme.txt"]
+
+let meme_chip treatment_sample =
+  best_peak_sequences ~qvalue:1e-10 ~radius:50 treatment_sample
+  |> Meme_suite.meme_chip
+    ~meme_nmotifs:3 ~meme_minw:5 ~meme_maxw:8
+
+let chipqc =
+  let samples = List.map all_of_chIP_sample ~f:(fun x -> {
+        ChIPQC.id = show_chIP_sample x ;
+        tissue = "yeast" ;
+        factor = show_factor (factor x) ;
+        replicate = "1" ;
+        bam = mapped_reads_bam x ;
+        peaks = Macs2.narrow_peaks (tf_peaks x) ;
+      })
+  in
+  ChIPQC.run samples
+
+let occdist_vs_peak_score treatment_sample : svg file =
+  let open Bistro.Shell_dsl in
+  let peaks = centered_tf_peaks ~radius:500 treatment_sample in
+  let sequences = Ucsc_gb.twoBitToFa genome_2bit (Bed.keep4 peaks) in
+  let occ = dep @@ Meme_suite.fimo (meme_motifs treatment_sample) sequences in
+  let peaks = dep peaks in
+  let script = [%script {|
+occ <- read.table("{{occ}}/fimo.txt", sep="\t",header=T, comment.char="")
+motifs <- sort(unique(occ$X.pattern.name))
+peaks <- read.table("{{peaks}}", sep="\t", col.names=c("chr","start","end","id","score"))
+peaks <- peaks[order(peaks$score, decreasing=T),]
+svg("{{dest}}", height=10)
+par(mfrow=c(length(motifs), 2))
+for (m in motifs) {
+  closest_occ <- sapply(peaks$id, function(p) {
+      o <- occ[occ$X.pattern.name == m & occ$sequence.name == as.character(p), ]
+      pos <- c(1000, (o$start + o$start + 1) / 2)
+      pos[which.min(abs(pos - 500))]
+    })
+  plot(peaks$score, closest_occ, main=sprintf("motif %d",m),xlab="Peak score",ylab="Closest occ")
+  close_occ <- abs(closest_occ - 500) < 100
+  print(summary(close_occ))
+  df <- data.frame(score = peaks$score, close_occ = close_occ)
+  g <- glm(close_occ ~ score,df, family="binomial")
+  plot(peaks$score, close_occ, xlab="Peak score", ylab="Close occ prob")
+  lines(peaks$score, predict(g,type="resp"))
+}
+dev.off()
+|}]
+  in
+  Bistro.Workflow.shell ~descr:"occdist_vs_peak_rank" [
+    cmd "Rscript" [ file_dump script ]
+  ]
+
+let report =
+  let open Bistro_utils.Html_report in
+  make
+    ~title:"Integrated approaches reveal determinants of genome-wide binding and function of the transcription factor Pho4."
+    [
+      text {|
+        This is an attempt at reproducing a paper by Zhou and O'Shea on why
+        transcription factors with similar binding sequences are not bound
+        to the same genomic sites.
+      |} ;
+      section "Inferred motifs" ;
+      png (Meme_suite.meme_logo (meme `ChIP_Pho4_noPi) 1) ;
+      svg (occdist_vs_peak_score `ChIP_Pho4_noPi) ;
+    ]
+  |> render
+
+let repo = Repo.[
+    item [ "report.html" ] report ;
+    item [ "macs2" ; "Pho4" ; "noPi" ] (tf_peaks `ChIP_Pho4_noPi) ;
+    item [ "meme" ; "Pho4" ; "noPi" ] (meme `ChIP_Pho4_noPi) ;
+    item [ "meme_chip" ; "Pho4" ; "noPi" ] (meme_chip `ChIP_Pho4_noPi) ;
+    item [ "chIP-QC" ; "Pho4" ; "noPi" ] chipqc ;
+    item [ "fastq-screen" ; "Pho4" ; "highPi" ] (fastq_screen `ChIP_Pho4_highPi) ;
+  ]
+
+let run () =
+  Repo.build_main
+    ~np ~mem:(`GB 4)
+    ~outdir:"res"
+    ~loggers:[ Console_logger.create () ]
+    repo
